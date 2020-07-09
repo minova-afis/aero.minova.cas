d91128111cdd41b148f5838916c4c9f52597674a
Stefan Wild <wild@minova.com>
2020-07-09 17:10:07 +0200

    Verschieben in Unterordner

--- aero.minova.core.application.system/src/main/java/aero/minova/core/application/system/controller/SqlController.java
+++ aero.minova.core.application.system/src/main/java/aero/minova/core/application/system/controller/SqlController.java
@@ -1,6 +1,5 @@
 package aero.minova.core.application.system.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;

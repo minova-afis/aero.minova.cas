32ba0772708e2ad49030d64b052d3c141798a84b
avots <avots@minova.de>
2020-07-09 14:54:59 +0200

    Tabellen-Namen definieren.

---
+++ src/test/java/aero/minova/core/application/system/controller/SqlControllerTest.java
@@ -0,0 +1,21 @@
+package aero.minova.core.application.system.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlControllerTest {
+	
+	@Autowired
+	SqlController testSubject;
+
+	@Test
+	void testIndexViewResultName() {
+		assertThat(testSubject.getIndexView("IndexViewName").getName()).isEqualTo("IndexViewName");
+	}
+
+}

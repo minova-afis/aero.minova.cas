0588a9b858e554e0c363e1fc8b108b29f3b59624
avots <avots@minova.de>
2020-07-09 14:54:59 +0200

    Tabellen-Namen definieren.

--- src/main/java/aero/minova/core/application/system/CoreApplicationSystemApplication.java
+++ src/main/java/aero/minova/core/application/system/CoreApplicationSystemApplication.java
@@ -4,14 +4,31 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import aero.minova.core.application.system.domain.Value;
+import aero.minova.core.application.system.domain.ValueDeserializer;
+import aero.minova.core.application.system.domain.ValueSerializer;
 
 // Der CompponentScan wird verwendet, damit alle Minova Komponenten im classpath geladen werden.
-@ComponentScan("ch.minova")
 @SpringBootApplication
+@Configuration
 public class CoreApplicationSystemApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(CoreApplicationSystemApplication.class, args);
+	}
+	
+	@Bean
+	public Gson gson() {
+		return new GsonBuilder()//
+				.registerTypeAdapter(Value.class, new ValueSerializer())//
+				.registerTypeAdapter(Value.class, new ValueDeserializer())//
+				.create();
 	}
 }
--- src/main/java/aero/minova/core/application/system/controller/SqlController.java
+++ src/main/java/aero/minova/core/application/system/controller/SqlController.java
@@ -2,6 +2,7 @@ package aero.minova.core.application.system.controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import aero.minova.core.application.system.domain.Table;
@@ -9,8 +10,9 @@ import aero.minova.core.application.system.domain.Table;
 @RestController
 public class SqlController {
 	@GetMapping(value = "data/index", produces = "application/json")
-	public Table getMovements(Model model) {
+	public Table getIndexView(@RequestParam String indexViewName) {
 		Table movementTable = new Table();
+		movementTable.setName(indexViewName);
 		return movementTable;
 	}
 }

2b7e4dd99f41e738497f0d7b2f29c247f7a81216
avots <avots@minova.de>
2020-07-09 16:16:11 +0200

    NCore entfernen.

--- src/main/java/aero/minova/core/application/system/controller/FilesController.java
+++ src/main/java/aero/minova/core/application/system/controller/FilesController.java
@@ -1,23 +1,14 @@
 package aero.minova.core.application.system.controller;
 
-import static ch.minova.ncore.util.MessageFormat.format;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.readAllBytes;
-import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
-
-import java.nio.file.Paths;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import ch.minova.ncore.util.MessageFormat;
 import lombok.var;
 
 @RestController
@@ -31,9 +22,8 @@ public class FilesController {
 	public String getFile(@RequestParam String path) throws Exception {
 		var inputPath = files.getSystemFolder().resolve(path).toAbsolutePath().normalize();
 		if (!inputPath.startsWith(files.getSystemFolder())) {
-			throw new IllegalAccessException(format("Path variable with value \"{0}\" points outside the shared data folder of the system.", path));
+			throw new IllegalAccessException("Path variable with value " + path + " points outside the shared data folder of the system.");
 		}
 		return new String(readAllBytes(inputPath), UTF_8);
 	}
-
 }

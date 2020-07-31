87dc19335db021c90bd95cb3e564f7643c8b59d2
avots <avots@minova.de>
2020-07-31 16:04:12 +0200

    #48022 Ersten Entwurf für Prozeduraufruf erstellen.

--- aero.minova.core.application.system/src/main/java/aero/minova/core/application/system/controller/SqlProcedureController.java
+++ aero.minova.core.application.system/src/main/java/aero/minova/core/application/system/controller/SqlProcedureController.java
@@ -1,13 +1,24 @@
 package aero.minova.core.application.system.controller;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import aero.minova.core.application.system.domain.Column;
+import aero.minova.core.application.system.domain.DataType;
+import aero.minova.core.application.system.domain.Row;
 import aero.minova.core.application.system.domain.Table;
+import aero.minova.core.application.system.domain.Value;
+import aero.minova.core.application.system.sql.ExecuteStrategy;
 import aero.minova.core.application.system.sql.SystemDatabase;
+import lombok.val;
 
 public class SqlProcedureController {
 	Connection sqlConnection;
@@ -17,7 +28,70 @@ public class SqlProcedureController {
 		if (sqlConnection == null) {
 			sqlConnection = SystemDatabase.connection();
 		}
-		throw new UnsupportedOperationException();
+		try {
+			val preparedStatement = sqlConnection.prepareCall(prepareProcedureString(inputTable));
+			IntStream.range(0, inputTable.getRows().get(0).getValues().size())//
+					.forEach(i -> {
+						try {
+							preparedStatement.setString(i, inputTable.getRows().get(0).getValues().get(i).getValue().toString());
+						} catch (SQLException e) {
+							throw new RuntimeException(e);
+						}
+					});
+			val resultSet = preparedStatement.executeQuery();
+			val outputTable = new Table();
+			outputTable.setName(inputTable.getName());
+			outputTable.setColumns(//
+					inputTable.getColumns().stream()//
+							.filter(column -> !Objects.equals(column.getName(), Column.AND_FIELD_NAME))//
+							.collect(Collectors.toList()));
+			while (resultSet.next()) {
+				Row row = new Row();
+				for (Column column : outputTable.getColumns()) {
+					// TODO Feld typisieren.
+					if (column.getType() == DataType.STRING) {
+						row.addValue(new Value(resultSet.getString(column.getName())));
+					} else if (column.getType() == DataType.INTEGER) {
+						row.addValue(new Value(resultSet.getInt(column.getName())));
+					} else {
+						throw new UnsupportedOperationException("Der Typ " + column.getType() + " wird nicht unterstützt.");
+					}
+				}
+				outputTable.addRow(row);
+			}
+			return outputTable;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
-	
+
+	private static String prepareProcedureString(Table params) {
+		return prepareProcedureString(params, ExecuteStrategy.standard);
+	}
+
+	/**
+	 * Bereitet einen Prozedur-String vor
+	 * 
+	 * @param name
+	 * @param fds
+	 * @param strategy
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	private static String prepareProcedureString(Table params, Set<ExecuteStrategy> strategy) throws IllegalArgumentException {
+		if (params.getName() == null || params.getName().trim().length() == 0) {
+			throw new IllegalArgumentException("Cannot prepare procedure with NULL name");
+		}
+		final int paramCount = params.getColumns().size();
+		final boolean returnRequired = ExecuteStrategy.returnRequired(strategy);
+
+		final StringBuffer sb = new StringBuffer();
+		sb.append('{').append(returnRequired ? "? = call " : "call ").append(params.getName()).append("(");
+		for (int i = 0; i < paramCount; i++) {
+			sb.append(i == 0 ? "?" : ",?");
+		}
+		sb.append(")}");
+		return sb.toString();
+	}
+
 }

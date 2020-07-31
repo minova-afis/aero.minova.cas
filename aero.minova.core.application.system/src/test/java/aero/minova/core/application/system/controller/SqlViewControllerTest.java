272ff64aab1f8917488e331d123a5d025d0d180f
avots <avots@minova.de>
2020-07-31 15:13:34 +0200

    #48022 Alle Tests beschriften.

--- aero.minova.core.application.system/src/test/java/aero/minova/core/application/system/controller/SqlViewControllerTest.java
+++ aero.minova.core.application.system/src/test/java/aero/minova/core/application/system/controller/SqlViewControllerTest.java
@@ -3,9 +3,11 @@ package aero.minova.core.application.system.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,6 +30,7 @@ class SqlViewControllerTest {
 		// assertThat(testSubject.getIndexView("IndexViewName").getName()).isEqualTo("IndexViewName");
 	}
 
+	@DisplayName("Wähle Einträge ohne Einschränkungen aus.")
 	@Test
 	void testPrepareViewString_withStarSelect() {
 		Table inputTable = new Table();
@@ -35,7 +38,8 @@ class SqlViewControllerTest {
 		assertThat(testSubject.prepareViewString(inputTable, true, 1000))//
 				.isEqualTo("select top 1000 * from vWorkingTimeIndex2");
 	}
-	
+
+	@DisplayName("Wähle alle Einträge mit einem bestimmten Wert eines Feldes.")
 	@Test
 	void testPrepareViewString_withSelectByOneAttributeValue() {
 		Table inputTable = new Table();
@@ -51,7 +55,8 @@ class SqlViewControllerTest {
 		assertThat(testSubject.prepareViewString(inputTable, true, 1000))//
 				.isEqualTo("select top 1000 * from vWorkingTimeIndex2\r\nwhere (EmployeeText like 'AVM%')");
 	}
-	
+
+	@DisplayName("Wähle alle Einträge mit jeweils einen bestimmten Werten in zwei Feldern.")
 	@Test
 	void testPrepareViewString_withSelectByMultipleAttributeValue() {
 		Table inputTable = new Table();
@@ -70,4 +75,28 @@ class SqlViewControllerTest {
 				.isEqualTo("select top 1000 * from vWorkingTimeIndex2\r\nwhere (EmployeeText like 'AVM%' and CustomerText like 'MIN%')");
 	}
 
+	@DisplayName("Wähle alle Einträge eines Datumsbereiches.")
+	@Test
+	void testPrepareViewString_withSelectByMultipleConditionsOnSameAttribute() {
+		Table inputTable = new Table();
+		inputTable.setName("vWorkingTimeIndex2");
+		inputTable.addColumn(new Column("BookingDate", DataType.STRING));
+		inputTable.addColumn(Column.AND_FIELD);
+		{
+			Row inputRow = new Row();
+			inputRow.addValue(new Value("<=" + LocalDate.of(2020, 7, 31).toString()));
+			inputRow.addValue(new Value(false));
+			inputTable.addRow(inputRow);
+		}
+		{
+			Row inputRow = new Row();
+			inputRow.addValue(new Value(">" + LocalDate.of(2020, 7, 29).toString()));
+			inputRow.addValue(new Value(true));
+			inputTable.addRow(inputRow);
+		}
+		assertThat(testSubject.prepareViewString(inputTable, true, 1000))//
+				.isEqualTo("select top 1000 * from vWorkingTimeIndex2\r\n" //
+						+ "where (BookingDate <= '2020-07-31')\r\n"//
+						+ "  and (BookingDate > '2020-07-29')");
+	}
 }

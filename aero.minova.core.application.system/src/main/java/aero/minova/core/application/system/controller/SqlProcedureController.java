395d1eb4a3bf76f99b195e564e09afb54bdde25e
avots <avots@minova.de>
2020-07-31 15:34:06 +0200

    #48022 Mapping für Prozeduraufruf erstellen.

---
+++ aero.minova.core.application.system/src/main/java/aero/minova/core/application/system/controller/SqlProcedureController.java
@@ -0,0 +1,29 @@
+package aero.minova.core.application.system.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import aero.minova.core.application.system.domain.Table;
+
+public class SqlProcedureController {
+	Connection sqlConnection;
+
+	private Connection msSqlConnection() {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			return DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=AFIS_HAM", "sa", "Minova+0");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@PostMapping(value = "data/procedure", produces = "application/json")
+	public Table executeProcedure(@RequestBody Table inputTable) {
+		if (sqlConnection == null) {
+			sqlConnection = msSqlConnection();
+		}
+	}
+}

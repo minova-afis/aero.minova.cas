3042160f6f9a395400f07eb957fa58e5c7b794e3
avots <avots@minova.de>
2020-09-21 09:24:02 +0200

    #20 Container für verschiedene Rückgabewerte erstellen.

---
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/domain/SqlProcedureResult.java
@@ -0,0 +1,10 @@
+package aero.minova.core.application.system.domain;
+
+import lombok.Data;
+
+@Data
+public class SqlProcedureResult {
+	private Table resultSet;
+	private Table outputParameters;
+	private Integer returnCode;
+}

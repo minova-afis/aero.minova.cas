1b1ab57e29060542f8e47fd8979fd8ba7933c306
avots <avots@minova.de>
2020-10-02 12:13:18 +0200

    #15 HTTPS aktivieren.

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/CoreApplicationSystemApplication.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/CoreApplicationSystemApplication.java
@@ -1,6 +1,7 @@
 package aero.minova.core.application.system;
 
 import java.io.IOException;
+import java.time.Instant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ aero.minova.core.application.system.service/src/test/java/aero/minova/core/application/system/controller/OTest.java
@@ -0,0 +1,14 @@
+package aero.minova.core.application.system.controller;
+
+import java.time.Instant;
+
+import org.junit.jupiter.api.Test;
+
+public class OTest {
+	
+	@Test
+	public void test() {
+		System.out.println(Instant.now());
+	}
+
+}

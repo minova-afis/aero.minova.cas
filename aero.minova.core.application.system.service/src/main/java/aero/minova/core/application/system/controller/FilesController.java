65835fb5cc57265808a0d32c0ecdda3d428f1d9d
avots <avots@minova.de>
2020-10-29 17:17:01 +0100

    Dateiinhalt als Bytestream zurückgeben.

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/FilesController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/FilesController.java
@@ -4,6 +4,8 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.readAllBytes;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,13 +19,12 @@ public class FilesController {
 	@Autowired
 	FilesService files;
 
-	@RequestMapping(value = "files/read")
-	@ResponseBody
-	public String getFile(@RequestParam String path) throws Exception {
+	@RequestMapping(value = "files/read", produces= {MediaType.APPLICATION_OCTET_STREAM_VALUE})
+	public @ResponseBody byte[] getFile(@RequestParam String path) throws Exception {
 		val inputPath = files.getSystemFolder().resolve(path).toAbsolutePath().normalize();
 		if (!inputPath.startsWith(files.getSystemFolder())) {
 			throw new IllegalAccessException("Path variable with value " + path + " points outside the shared data folder of the system.");
 		}
-		return new String(readAllBytes(inputPath), UTF_8);
+		return readAllBytes(inputPath);
 	}
 }

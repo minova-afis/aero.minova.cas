f54fed8838e289b6060dfb2f44d6f75b5fd647db
avots <avots@minova.de>
2020-11-19 09:47:16 +0100

    #142 Ping-Befehl implementieren.

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/SqlProcedureController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/SqlProcedureController.java
@@ -15,6 +15,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,16 @@ public class SqlProcedureController {
 	TracController trac;
 
 	private final ObjectMapper objectMapper = new ObjectMapper();
+
+	/**
+	 * Hiermit kann geprüft werden, ob die Kommunikation mit und die Anmeldung an den CAS funktioniert.
+	 */
+	@GetMapping(value = "ping", produces = "application/json")
+	public SqlProcedureResult executePing() {
+		val result = new SqlProcedureResult();
+		result.setReturnCode(0);
+		return result;
+	}
 
 	@PostMapping(value = "data/procedure", produces = "application/json")
 	public SqlProcedureResult executeProcedure(@RequestBody Table inputTable) {

35c7019497a745736df7ed2021faefd9abbf3492
avots <avots@minova.de>
2020-11-30 14:06:37 +0100

    Ping Serialisierungs-Fehler beheben.

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/domain/PingResponse.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/domain/PingResponse.java
@@ -2,4 +2,8 @@ package aero.minova.core.application.system.domain;
 
 public class PingResponse {
 	private final Integer returnCode = 0;
+
+	public Integer getReturnCode() {
+		return returnCode;
+	}
 }

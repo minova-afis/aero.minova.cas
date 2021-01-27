779c7bd16c8d600c76f989b98fb08e8066ec7cc4
Kerstin Weber <weber@minova.aero>
2021-01-27 13:49:07 +0100

    #53 Tabellen und View entfernen

--- vCASUserPrivileges.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW vCASUserPrivileges
-with encryption AS
-select UP.KeyText  as PrivilegeKeyText, UG.KeyText as KeyText, UG.RowLevelSecurity
-from 
-tUserPrivilege UP
-join tLuUserPrivilegeUserGroup UPG on UP.KeyLong=UPG.UserPrivilegeKey
-join tUserGroup UG on UG.KeyLong=UPG.UserGroupKey

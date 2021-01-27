29aba77c9d888cf5f947675a80e32fbdf7f4bb08
avots <avots@minova.de>
2021-01-27 17:27:34 +0100

    #54 Wurzelverzeichniss konfigurierbar machen.

--- aero.minova.core.application.system.service/src/test/java/aero/minova/core/application/system/controller/FileControllerTest.java
+++ aero.minova.core.application.system.service/src/test/java/aero/minova/core/application/system/controller/FileControllerTest.java
@@ -1,6 +1,6 @@
 package aero.minova.core.application.system.controller;
 
-import static java.nio.file.Files.createDirectory;
+import static java.nio.file.Files.createDirectories;
 import static java.nio.file.Files.write;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,8 +8,8 @@ import java.nio.charset.StandardCharsets;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -21,24 +21,39 @@ import lombok.val;
 @ActiveProfiles("test")
 public class FileControllerTest {
 
-	@Autowired
-	FilesService files;
-
-	@Autowired
-	FilesController testSubject;
-
 	@Test
 	public void testLegal() throws Exception {
-		val afisFolder = files.applicationFolder("AFIS");
-		createDirectory(afisFolder);
-		write(afisFolder.resolve("AFIS.xbs"), new String("<preferences></preferences>").getBytes(StandardCharsets.UTF_8));
+		final val rootPath = new TemporaryFolder();
+		rootPath.create();
+		final val rootFolder = rootPath.getRoot().toPath();
+		final val sharedDataFolder = rootPath.newFolder("Shared Data").toPath();
+		final val programFilesFolder = sharedDataFolder.resolve("Program Files");
+		final val serviceFolder = programFilesFolder.resolve("AFIS");
+		createDirectories(serviceFolder);
+
+		final val testSubject = new FilesController();
+		testSubject.files = new FilesService(rootFolder.toString());
+		testSubject.files.setUp();
+
+		write(programFilesFolder.resolve("AFIS").resolve("AFIS.xbs"), new String("<preferences></preferences>").getBytes(StandardCharsets.UTF_8));
 		assertThat(testSubject.getFile("Shared Data/Program Files/AFIS/AFIS.xbs")).isEqualTo("<preferences></preferences>".getBytes(StandardCharsets.UTF_8));
 	}
 
 	@Test
-	public void testIllegal() {
-		Assertions.assertThrows(IllegalAccessException.class,
-				() -> assertThat(testSubject.getFile("../Shared Data/Program Files/AFIS/AFIS.xbs")));
+	public void testIllegal() throws Exception {
+		final val rootPath = new TemporaryFolder();
+		rootPath.create();
+		final val rootFolder = rootPath.getRoot().toPath();
+		final val sharedDataFolder = rootPath.newFolder("Shared Data").toPath();
+		final val programFilesFolder = sharedDataFolder.resolve("Program Files");
+		final val serviceFolder = programFilesFolder.resolve("AFIS");
+		createDirectories(serviceFolder);
+
+		final val testSubject = new FilesController();
+		testSubject.files = new FilesService(rootFolder.toString());
+		testSubject.files.setUp();
+
+		Assertions.assertThrows(IllegalAccessException.class, () -> assertThat(testSubject.getFile("../Shared Data/Program Files/AFIS/AFIS.xbs")));
 	}
 
 }
--- aero.minova.core.application.system.service/src/test/java/aero/minova/core/application/system/controller/FilesControllerTestConfiguration.java
+++ aero.minova.core.application.system.service/src/test/java/aero/minova/core/application/system/controller/FilesControllerTestConfiguration.java
@@ -1,44 +1,8 @@
 package aero.minova.core.application.system.controller;
 
-import static java.nio.file.Files.createDirectories;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-import org.junit.rules.TemporaryFolder;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-
-import lombok.val;
 
 @TestConfiguration
 @Profile("test")
-public class FilesControllerTestConfiguration {
-
-	private final Path programFilesFolder;
-	private final Path serviceFolder;
-	private final Path sharedDataFolder;
-
-	public FilesControllerTestConfiguration() throws IOException {
-		val folder = new TemporaryFolder();
-		folder.create();
-		sharedDataFolder = folder.newFolder("Shared Data").toPath();
-		programFilesFolder = sharedDataFolder.resolve("Program Files");
-		serviceFolder = programFilesFolder.resolve("core.application.system");
-		createDirectories(serviceFolder);
-	}
-
-	@Bean
-	@Primary
-	public FilesService filesServices() {
-		return new FilesService(serviceFolder);
-	}
-
-	@Bean
-	@Primary
-	public FilesController filesController() {
-		return new FilesController();
-	}
-}
+public class FilesControllerTestConfiguration {}

857182888b01b927a70fa6989b3cb9c802f920f4
Kerstin Weber <weber@minova.aero>
2021-02-12 10:10:18 +0100

    #62 Value erweitert um rule und Controller um die dazugehörige Funktionalität

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/domain/Value.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/domain/Value.java
@@ -13,39 +13,90 @@ import java.time.ZonedDateTime;
 public class Value {
 	private DataType type;
 	private Object value;
+	private String rule;
 
 	public Value(Integer integerValue) {
 		type = DataType.INTEGER;
+		rule = null;
 		setValue(requireNonNull(integerValue));
 	}
 
 	public Value(Long longValue) {
 		type = DataType.LONG;
+		rule = null;
 		setValue(longValue);
 	}
 
 	public Value(Boolean booleanValue) {
 		type = DataType.BOOLEAN;
+		rule = null;
 		setValue(booleanValue);
 	}
 
 	public Value(Double doubleValue) {
 		type = DataType.DOUBLE;
+		rule = null;
 		setValue(doubleValue);
 	}
 
 	public Value(String stringValue) {
 		type = DataType.STRING;
+		rule = null;
 		setValue(stringValue);
 	}
 
 	public Value(Instant instantValue) {
 		type = DataType.INSTANT;
+		rule = null;
 		setValue(instantValue);
+
 	}
 
 	public Value(ZonedDateTime zonedDateTimeValue) {
 		type = DataType.ZONED;
+		rule = null;
+		setValue(zonedDateTimeValue);
+	}
+
+	public Value(Integer integerValue, String rule) {
+		type = DataType.INTEGER;
+		this.rule = rule;
+		setValue(requireNonNull(integerValue));
+	}
+
+	public Value(Long longValue, String rule) {
+		type = DataType.LONG;
+		this.rule = rule;
+		setValue(longValue);
+	}
+
+	public Value(Boolean booleanValue, String rule) {
+		type = DataType.BOOLEAN;
+		this.rule = rule;
+		setValue(booleanValue);
+	}
+
+	public Value(Double doubleValue, String rule) {
+		type = DataType.DOUBLE;
+		this.rule = rule;
+		setValue(doubleValue);
+	}
+
+	public Value(String stringValue, String rule) {
+		type = DataType.STRING;
+		this.rule = rule;
+		setValue(stringValue);
+	}
+
+	public Value(Instant instantValue, String rule) {
+		type = DataType.INSTANT;
+		this.rule = rule;
+		setValue(instantValue);
+	}
+
+	public Value(ZonedDateTime zonedDateTimeValue, String rule) {
+		type = DataType.ZONED;
+		this.rule = rule;
 		setValue(zonedDateTimeValue);
 	}
 
@@ -56,6 +107,18 @@ public class Value {
 
 	public DataType getType() {
 		return type;
+	}
+
+	public void setType(DataType type) {
+		this.type = type;
+	}
+
+	public String getRule() {
+		return rule;
+	}
+
+	public void setRule(String rule) {
+		this.rule = rule;
 	}
 
 	public Object getValue() {

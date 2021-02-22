c17cce845ff726306d81bcd93d607f3984236529
avots <avots@minova.de>
2021-02-22 11:24:25 +0200

    #72 Docker-Konfiguration anhand von aero.minova.demo vereinfachen.

--- aero.minova.core.application.system.service/build.as.docker.image.sh
+++ aero.minova.core.application.system.service/build.as.docker.image.sh
@@ -4,6 +4,13 @@ mvn clean verify
 mvn package spring-boot:repackage
 #cp target/core.application.system-12.4.0-SNAPSHOT.jar \
 #	../aero.minova.core.application.system.docker/core.application.system.jar
-docker build --file=Dockerfile.server \
-	--tag=aero.minova.cas:latest --rm=true .
+docker build --tag=aero.minova.cas:latest --rm=true .
+echo This command also contains some helpful commands, in its source code.
+exit
+
 echo docker run --name=aero.minova.cas --publish=8084:8084 --network="host" aero.minova.cas:latest
+echo List all running containers: docker ps
+echo Stop docker docker stop <container id|name>
+echo docker container ls
+echo docker container rm <container id|name>
+echo Remove all containers: docker system prune

e4ebd0fbc804cf3d4b087d333d202c21ae3ea835
avots <avots@minova.de>
2021-02-24 18:42:51 +0200

    aero.minova.cas.logic importieren.

---
+++ aero.minova.cas.logic/src/aero/minova/cas/logic/sql/xpcasInsertUserGroup.sql
@@ -0,0 +1,30 @@
+alter procedure dbo.xpcasInsertUserGroup (
+	@KeyLong int output,
+	@KeyText nvarchar(10) = null,
+	@Description nvarchar(50) = null,
+	@UserCode nvarchar(50) = null,
+	@SecurityToken nvarchar(250) = null
+)
+with encryption as
+	if (exists(select * from xtcasUserGroup
+		where KeyText = @KeyText
+		  and LastAction > 0))
+	begin
+		raiserror('ADO | 25 | msg.sql.DuplicateMatchcodeNotAllowed', 16, 1) with seterror
+		return -1
+	end
+
+	insert into xtcasUserGroup (
+		KeyText,
+		Description,
+		UserCode,
+		SecurityToken
+	) values (
+		@KeyText,
+		@Description,
+		@UserCode,
+		@SecurityToken
+	)
+
+	select @KeyLong = @@identity
+return @@error

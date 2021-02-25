aec0c0ef110f4e9ed7c51b98066c26836175b0ee
avots <avots@minova.de>
2021-02-25 12:12:20 +0200

    #70 Verweise alte Tabelle aktualisieren.

--- aero.minova.core.application.system.service/src/main/resources/sql/xvcasUserPrivileges.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW xvcasUserPrivileges
-with encryption AS
-select UP.KeyLong, UP.KeyText as PrivilegeKeyText, UG.KeyText as KeyText, UPG.RowLevelSecurity
-from 
-xtcasUserPrivilege UP
-join xtcasLuUserPrivilegeUserGroup UPG on UP.KeyLong=UPG.UserPrivilegeKey
-join xtcasUserGroup UG on UG.KeyLong=UPG.UserGroupKey

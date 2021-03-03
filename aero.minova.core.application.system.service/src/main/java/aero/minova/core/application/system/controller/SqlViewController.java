7f5a0e4814a70856c7390e90c11ee8b57346ffe1
Kerstin Weber <weber@minova.aero>
2021-03-03 07:33:53 +0100

    Entfernen des Substring in getPrivilegePermissions

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/SqlViewController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/SqlViewController.java
@@ -204,7 +204,7 @@ public class SqlViewController {
 
 		List<String> userTokens = new ArrayList<>();
 		for (GrantedAuthority ga : securityTokens) {
-			userTokens.add(ga.getAuthority().substring(5));
+			userTokens.add(ga.getAuthority());
 		}
 
 		for (String s : userTokens) {

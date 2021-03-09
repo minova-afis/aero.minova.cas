abca14a4199bde111594502fe061ce2a5df999b0
Kerstin Weber <weber@minova.aero>
2021-03-09 11:43:46 +0100

    Überarbeiten RückgabeFormat und Hinzufügen von übersetzbaren Fehlern

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/ControllerExceptionHandler.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/ControllerExceptionHandler.java
@@ -14,8 +14,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import aero.minova.core.application.system.domain.Column;
+import aero.minova.core.application.system.domain.DataType;
 import aero.minova.core.application.system.domain.ErrorMessage;
+import aero.minova.core.application.system.domain.Row;
 import aero.minova.core.application.system.domain.Table;
+import aero.minova.core.application.system.domain.Value;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
@@ -23,65 +27,58 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler(IllegalArgumentException.class)
 	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 	public Table illegalArgumentException(IllegalArgumentException ex, WebRequest request) {
-		Table t = new Table();
-		Exception sqlE = new Exception("Couldn't execute query: " + ex.getMessage(), ex);
-		ErrorMessage error = new ErrorMessage();
-		error.setErrorMessage(sqlE);
-		StringWriter sw = new StringWriter();
-		PrintWriter pw = new PrintWriter(sw);
-		ex.printStackTrace(pw);
-		String messages = sw.toString();
-		List<String> trace = Stream.of(messages.split("\n\tat|\n"))//
-				.map(String::trim)//
-				.collect(Collectors.toList());
-		error.setTrace(trace);
-		t.setReturnErrorMessage(error);
-		return t;
+		return prepareExceptionReturnTable(ex);
 	}
 
 	@ExceptionHandler(UnsupportedOperationException.class)
 	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 	public Table unsupportedOperationException(UnsupportedOperationException ex, WebRequest request) {
-		Table t = new Table();
-		Exception sqlE = new Exception("Couldn't execute query: " + ex.getMessage(), ex);
-		ErrorMessage error = new ErrorMessage();
-		error.setErrorMessage(sqlE);
-		StringWriter sw = new StringWriter();
-		PrintWriter pw = new PrintWriter(sw);
-		ex.printStackTrace(pw);
-		String messages = sw.toString();
-		List<String> trace = Stream.of(messages.split("\n\tat|\n"))//
-				.map(String::trim)//
-				.collect(Collectors.toList());
-		error.setTrace(trace);
-		t.setReturnErrorMessage(error);
-		return t;
+		return prepareExceptionReturnTable(ex);
 	}
 
 	@ExceptionHandler(RuntimeException.class)
 	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 	public Table runtimeException(RuntimeException ex, WebRequest request) {
-		Table t = new Table();
-		Exception sqlE = new Exception("Couldn't execute query: " + ex.getMessage(), ex);
-		ErrorMessage error = new ErrorMessage();
-		error.setErrorMessage(sqlE);
-		StringWriter sw = new StringWriter();
-		PrintWriter pw = new PrintWriter(sw);
-		ex.printStackTrace(pw);
-		String messages = sw.toString();
-		List<String> trace = Stream.of(messages.split("\n\tat|\n"))//
-				.map(String::trim)//
-				.collect(Collectors.toList());
-		error.setTrace(trace);
-		t.setReturnErrorMessage(error);
-		return t;
+		return prepareExceptionReturnTable(ex);
 	}
 
 	@ExceptionHandler(SQLException.class)
 	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 	public Table sQLException(SQLException ex, WebRequest request) {
-		Table t = new Table();
-		Exception sqlE = new Exception("Couldn't execute query: " + ex.getMessage(), ex);
+		return prepareExceptionReturnTable(ex);
+	}
+
+	private Table prepareExceptionReturnTable(Exception ex) {
+		Table outputTable = new Table();
+		outputTable.setName("Error");
+		outputTable.addColumn(new Column("International Message", DataType.STRING));
+		String errorMessage = ex.getMessage();
+
+		if (errorMessage == null) {
+			errorMessage = "msg.NoErrorMessageAvailable";
+		}
+		// falls die Message in der Form 'msg.Beispiel %ParameterDerInDieMessageNachDemÜbersetzenEingefügtWird' ist
+		List<String> errorMessageParts = Stream.of(errorMessage.split("%"))//
+				.map(String::trim)//
+				.collect(Collectors.toList());
+
+		// Alle Spalten müssen erstellt werden BEVOR sie befüllt werden
+		// Hinzufügen der Spalten für die InputParameter der Internationalierung
+		for (int i = 1; i < errorMessageParts.size(); i++) {
+			outputTable.addColumn(new Column("MessageInputParam" + i, DataType.STRING));
+		}
+
+		Row internatMsg = new Row();
+
+		// Hinzufügen der International Message und der InputParameter der Internationalierung
+		for (int i = 0; i < errorMessageParts.size(); i++) {
+			Value param = new Value(errorMessageParts.get(i), null);
+			internatMsg.addValue(param);
+		}
+
+		outputTable.addRow(internatMsg);
+
+		Exception sqlE = new Exception("Couldn't execute query: " + errorMessage, ex);
 		ErrorMessage error = new ErrorMessage();
 		error.setErrorMessage(sqlE);
 		StringWriter sw = new StringWriter();
@@ -92,7 +89,11 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 				.map(String::trim)//
 				.collect(Collectors.toList());
 		error.setTrace(trace);
-		t.setReturnErrorMessage(error);
-		return t;
+		outputTable.setReturnErrorMessage(error);
+		return outputTable;
+	}
+
+	private void saveErrorInDatabase() {
+
 	}
 }

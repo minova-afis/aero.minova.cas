83f6bc4419060e9174b2da9b5619c3265b9661ba
Kerstin Weber <weber@minova.aero>
2021-03-15 09:45:12 +0100

    Hinzufügen hashAll-Methode

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/FilesController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/FilesController.java
@@ -9,16 +9,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.val;
 
 @RestController
+@DependsOn("FilesService")
 public class FilesController {
 
 	@Autowired
@@ -64,14 +68,34 @@ public class FilesController {
 		return String.format(fx, new BigInteger(1, md.digest())).getBytes(StandardCharsets.UTF_8);
 	}
 
+	/*
+	 * wird zwar bei Programmstart ausgeführt, aber durch die DependsOn-Notation müsste es erst nach der setup-Methode von der FilesService Klasse aufgerufen
+	 * werden
+	 */
+	@PostConstruct
+	public void hashAll() throws IOException {
+		List<String> programFiles = files.populateFilesList(files.getProgramFilesFolder().toFile());
+		for (String path : programFiles) {
+			String fileSuffix = path.substring(path.lastIndexOf(".") + 1, path.length());
+			// wir wollen nicht noch einen Hash von einer gehashten Datei
+			if (!fileSuffix.toLowerCase().equals("md5")) {
+				byte[] hashOfFile = hashFile(Paths.get(path));
+				File hashedFile = new File(path.substring(0, path.lastIndexOf(".")) + ".md5");
+				// erzeugt auch die Datei, falls sie noch nicht existiert und überschreibt sie, falls sie schon exisitert
+				Files.write(Paths.get(path), hashOfFile);
+			}
+
+		}
+	}
+
 	@RequestMapping(value = "files/zip", produces = { MediaType.APPLICATION_OCTET_STREAM_VALUE })
 	public @ResponseBody byte[] getZip(@RequestParam String path) throws Exception {
 		val inputPath = files.getSystemFolder().resolve(path).toAbsolutePath().normalize();
 		if (!inputPath.startsWith(files.getSystemFolder())) {
 			throw new IllegalAccessException("msg.PathError %" + path + " %" + inputPath);
 		}
-		List<String> fileList = populateFilesList(new File(path));
-
+		List<String> fileList = files.populateFilesList(new File(path));
+		// Name des Ordners herausfinden, um das ZIP später so nennen zu können
 		String dirName = path.substring(path.lastIndexOf(File.separatorChar) + 1, path.length());
 		File[] files = Paths.get(path.substring(0, path.lastIndexOf(File.separatorChar) + 1)).toFile().listFiles();
 		boolean zipExists = false;
@@ -91,24 +115,21 @@ public class FilesController {
 	}
 
 	public void zip(String source, File zipFile, List<String> fileList) throws RuntimeException {
-		byte[] buffer = new byte[1024];
 		ZipEntry ze = null;
 		try {
-			// now zip files one by one
-			// create ZipOutputStream to write to the byte array
+			// jede Datei wird einzeln zu dem ZIP hinzugefügt
 			FileOutputStream fos = new FileOutputStream(zipFile);
 			ZipOutputStream zos = new ZipOutputStream(fos);
 			for (String filePath : fileList) {
 				System.out.println("Zipping " + filePath);
-				// for ZipEntry we need to keep only relative file path, so we used substring on absolute path
 				ze = new ZipEntry(filePath.substring(source.length() + 1, filePath.length()));
 				if (!ze.getName().equals(zipFile.getName())) {
 					zos.putNextEntry(ze);
 				}
 
-				// read the file and write to ZipOutputStream
 				FileInputStream fis = new FileInputStream(filePath);
 				int len;
+				byte[] buffer = new byte[1024];
 				BufferedInputStream entryStream = new BufferedInputStream(fis, 2048);
 				while ((len = entryStream.read(buffer, 0, 1024)) != -1) {
 					zos.write(buffer, 0, len);
@@ -123,22 +144,4 @@ public class FilesController {
 		}
 	}
 
-	/**
-	 * This method populates all the files in a directory to a List
-	 * 
-	 * @param dir
-	 * @throws IOException
-	 */
-	private List<String> populateFilesList(File dir) throws IOException {
-		List<String> filesListInDir = new ArrayList<String>();
-		File[] files = dir.listFiles();
-		for (File file : files) {
-			if (file.isFile()) {
-				filesListInDir.add(file.getAbsolutePath());
-			} else {
-				populateFilesList(file);
-			}
-		}
-		return filesListInDir;
-	}
 }

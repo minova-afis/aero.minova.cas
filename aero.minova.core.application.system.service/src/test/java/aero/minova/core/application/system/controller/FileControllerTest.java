f214a71456aa7258d8e97ba5c16c5ae40e44de0b
Kerstin Weber <weber@minova.aero>
2021-03-15 09:45:12 +0100

    Hinzufügen hashAll-Methode

--- aero.minova.core.application.system.service/src/test/java/aero/minova/core/application/system/controller/FileControllerTest.java
+++ aero.minova.core.application.system.service/src/test/java/aero/minova/core/application/system/controller/FileControllerTest.java
@@ -189,4 +189,43 @@ public class FileControllerTest {
 		Assertions.assertThrows(IllegalAccessException.class, () -> assertThat(testSubject.getZip("../Shared Data/Program Files/AFIS/AFIS.xbs")));
 	}
 
+	@Test
+	public void testLegalHashAllExists() throws Exception {
+		final val rootPath = new TemporaryFolder();
+		rootPath.create();
+		final val rootFolder = rootPath.getRoot().toPath();
+		final val sharedDataFolder = rootPath.newFolder("Shared Data").toPath();
+		final val programFilesFolder = sharedDataFolder.resolve("Program Files");
+		final val serviceFolder = programFilesFolder.resolve("AFIS");
+		createDirectories(serviceFolder);
+
+		final val testSubject = new FilesController();
+		testSubject.files = new FilesService(rootFolder.toString());
+		testSubject.files.setUp();
+
+		write(programFilesFolder.resolve("AFIS").resolve("AFIS.xbs"), new String("<preferences></preferences>").getBytes(StandardCharsets.UTF_8));
+		write(programFilesFolder.resolve("AFIS").resolve("AFIS.zip"), new String("").getBytes(StandardCharsets.UTF_8));
+
+		byte[] zipped = testSubject.getZip(serviceFolder.toString());
+
+		final byte[] buffer = new byte[1024];
+		ByteArrayInputStream bais = new ByteArrayInputStream(zipped);
+		int i = zipped.length;
+		final ZipInputStream zip = new ZipInputStream(bais);
+		final ByteArrayOutputStream out = new ByteArrayOutputStream(zipped.length);
+		ZipEntry ze = null;
+		int len = 0;
+		if ((ze = zip.getNextEntry()) != null) {
+			while ((len = zip.read(buffer)) != -1) {
+				out.write(buffer, 0, len);
+			}
+		}
+		zip.close();
+		out.close();
+		byte[] unzipped = out.toByteArray();
+		assertThat(readAllBytes(programFilesFolder.resolve("AFIS").resolve("AFIS.xbs"))).isEqualTo(unzipped);
+		assertThat(readAllBytes(programFilesFolder.resolve("AFIS").resolve("AFIS.zip"))).isNotEqualTo(new String("").getBytes(StandardCharsets.UTF_8));
+		assertThat(unzipped).isEqualTo("<preferences></preferences>".getBytes(StandardCharsets.UTF_8));
+	}
+
 }

e7dad31b2d9b27cb1cada231232516f3ca3917ed
Kerstin Weber <weber@minova.aero>
2021-03-16 12:43:41 +0100

    #93, #94 funktionierende Tests, aber noch kein zip und hash bei Startup

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/FilesController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/FilesController.java
@@ -18,6 +18,8 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,7 @@ public class FilesController {
 
 	@Autowired
 	FilesService files;
+	Logger logger = LoggerFactory.getLogger(SqlViewController.class);
 
 	@RequestMapping(value = "files/read", produces = { MediaType.APPLICATION_OCTET_STREAM_VALUE })
 	public @ResponseBody byte[] getFile(@RequestParam String path) throws Exception {
@@ -64,18 +67,52 @@ public class FilesController {
 		return String.format(fx, new BigInteger(1, md.digest())).getBytes(StandardCharsets.UTF_8);
 	}
 
+	// je höher die Zahl bei @Order, desto später wird die Methode ausgeführt
+//	@EventListener(ApplicationReadyEvent.class)
+//	@Order(2)
+	@RequestMapping(value = "files/hashAll")
 	public void hashAll() throws IOException {
-		List<String> programFiles = files.populateFilesList(files.getProgramFilesFolder().toFile());
+		List<String> programFiles = files.populateFilesList(files.getSystemFolder().toFile());
 		for (String path : programFiles) {
 			String fileSuffix = path.substring(path.lastIndexOf(".") + 1, path.length());
-			// wir wollen nicht noch einen Hash von einer gehashten Datei
-			if (!fileSuffix.toLowerCase().equals("md5")) {
+			// bevor die Dateien gehashed werden, werden sie gezipped (siehe @Order)
+			if (fileSuffix.toLowerCase().equals("zip")) {
+				String filePrefix = path.substring(0, path.lastIndexOf("."));
+				fileSuffix = path.substring(filePrefix.lastIndexOf(".") + 1, path.length());
+			}
+			// wir wollen nicht noch einen Hash von einer gehashten Datei und auch keinen Hash von einem Directory
+			if ((!fileSuffix.toLowerCase().contains("md5")) && (!new File(path).isDirectory())) {
 				byte[] hashOfFile = hashFile(Paths.get(path));
 				File hashedFile = new File(path + ".md5");
-				// erzeugt auch die Datei, falls sie noch nicht existiert und überschreibt sie, falls sie schon exisitert
+				// erzeugt die Datei, falls sie noch nicht existiert und überschreibt sie, falls sie schon exisitert
+				logger.info("Hashing: " + hashedFile.getAbsolutePath());
 				Files.write(Paths.get(hashedFile.getAbsolutePath()), hashOfFile);
 			}
 
+		}
+	}
+
+	// je niedriger die Zahl bei @Order, desto früher wird die Methode ausgeführt
+//	@EventListener(ApplicationReadyEvent.class)
+//	@Order(1)
+	@RequestMapping(value = "files/zipAll")
+	public void zipAll() throws Exception {
+		List<String> programFiles = files.populateFilesList(files.getSystemFolder().toFile());
+		for (String path : programFiles) {
+			String fileSuffix = path.substring(path.lastIndexOf(".") + 1, path.length());
+			// es kann sein, dass von einem vorherigen Start bereits gezippte Dateien vorhanden sind, welche schon gehashed wurden
+			if (fileSuffix.toLowerCase().equals("md5")) {
+				String filePrefix = path.substring(0, path.lastIndexOf("."));
+				fileSuffix = path.substring(filePrefix.lastIndexOf(".") + 1, path.length());
+			}
+			// wir wollen nicht noch einen zip von einer zip Datei
+			if ((!fileSuffix.toLowerCase().contains("zip")) && (new File(path)).isDirectory()) {
+				byte[] zipDataOfFile = getZip(path);
+				File zippedFile = new File(path + ".zip");
+				logger.info("Zipping: " + zippedFile.getAbsolutePath());
+				// erzeugt die Datei, falls sie noch nicht existiert und überschreibt sie, falls sie schon exisitert
+				Files.write(Paths.get(zippedFile.getAbsolutePath()), zipDataOfFile);
+			}
 		}
 	}
 
@@ -86,24 +123,11 @@ public class FilesController {
 			throw new IllegalAccessException("msg.PathError %" + path + " %" + inputPath);
 		}
 		List<String> fileList = files.populateFilesList(new File(path));
-		// Name des Ordners herausfinden, um das ZIP später so nennen zu können
-		String dirName = path.substring(path.lastIndexOf(File.separatorChar) + 1, path.length());
-		File[] files = Paths.get(path.substring(0, path.lastIndexOf(File.separatorChar) + 1)).toFile().listFiles();
-		boolean zipExists = false;
-		File zipFile = Paths.get(path).toFile();
-		for (File file : files) {
-			if (file.getName().equals(dirName + ".zip")) {
-				zipExists = true;
-				zipFile = file;
-			}
-		}
-		if (!zipExists) {
-			zipFile = new File(dirName + ".zip");
-		}
-//		Files.write(Paths.get(path), hashOfFile);
+		File zipFile = new File(path + ".zip");
+		// erzeugt Datei, falls sie noch nicht existiert, macht ansonsten nichts
+		zipFile.createNewFile();
 		zip(path.toString(), zipFile, fileList);
-		byte[] output = readAllBytes(Paths.get(zipFile.getAbsolutePath()));
-		return output;
+		return readAllBytes(Paths.get(zipFile.getAbsolutePath()));
 	}
 
 	public void zip(String source, File zipFile, List<String> fileList) throws RuntimeException {
@@ -113,21 +137,24 @@ public class FilesController {
 			FileOutputStream fos = new FileOutputStream(zipFile);
 			ZipOutputStream zos = new ZipOutputStream(fos);
 			for (String filePath : fileList) {
-				System.out.println("Zipping " + filePath);
-				ze = new ZipEntry(filePath.substring(source.length() + 1, filePath.length()));
-				if (!ze.getName().equals(zipFile.getName())) {
-					zos.putNextEntry(ze);
-				}
 
-				FileInputStream fis = new FileInputStream(filePath);
-				int len;
-				byte[] buffer = new byte[1024];
-				BufferedInputStream entryStream = new BufferedInputStream(fis, 2048);
-				while ((len = entryStream.read(buffer, 0, 1024)) != -1) {
-					zos.write(buffer, 0, len);
+				if (new File(filePath).isFile()) {
+					ze = new ZipEntry(filePath.substring(source.length() + 1, filePath.length()));
+					if (!ze.getName().equals(zipFile.getName())) {
+						zos.putNextEntry(ze);
+					}
+
+					// jeder Eintrag wird nacheinander in die ZIP Datei geschrieben mithilfe eines Buffers
+					FileInputStream fis = new FileInputStream(filePath);
+					int len;
+					byte[] buffer = new byte[1024];
+					BufferedInputStream entryStream = new BufferedInputStream(fis, 2048);
+					while ((len = entryStream.read(buffer, 0, 1024)) != -1) {
+						zos.write(buffer, 0, len);
+					}
+					zos.closeEntry();
+					fis.close();
 				}
-				zos.closeEntry();
-				fis.close();
 			}
 			zos.close();
 			fos.close();

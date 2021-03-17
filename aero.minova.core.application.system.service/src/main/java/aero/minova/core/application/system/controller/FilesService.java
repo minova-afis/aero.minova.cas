ef9f98de437abe79af9b37a4c2da839b20262fc7
Kerstin Weber <weber@minova.aero>
2021-03-17 10:16:01 +0100

    kleine Korrektur

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/FilesService.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/FilesService.java
@@ -14,11 +14,9 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("!test")
 public class FilesService {
 
 	@Value("${aero_minova_core_application_root_path:../../..}")

7c1999b520e209d4c31346e645b90e9b0f82abf4
avots <avots@minova.de>
2021-04-02 10:57:43 +0200

    #7 Adjust meeting time API access.

--- aero.minova.core.application.system.service/src/main/java/aero/minova/covid/test/print/MeetingController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/covid/test/print/MeetingController.java
@@ -5,13 +5,13 @@ import static java.util.Arrays.asList;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MeetingController {
-	@PostMapping(value = "meeting/time/available", produces = "application/json")
+	@GetMapping(value = "meeting/time/available", produces = "application/json")
 	public List<Timeslot> getAvailableTimeslot(@RequestBody TimeLocation timeLocation) throws Exception {
 		return asList(new Timeslot(LocalDateTime.of(2021, 4, 1, 9, 0), LocalDateTime.of(2021, 4, 1, 9, 30)), //
 				new Timeslot(LocalDateTime.of(2021, 4, 1, 9, 30), LocalDateTime.of(2021, 4, 1, 10, 0)), //

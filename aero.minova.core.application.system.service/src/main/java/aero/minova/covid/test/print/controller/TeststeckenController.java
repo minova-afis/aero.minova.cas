8c28eb4baa97e166d1c3b79555a5fbadd5c5281a
avots <avots@minova.de>
2021-04-02 12:30:58 +0200

    #7 Teststrecken API erstellen.

---
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/covid/test/print/controller/TeststeckenController.java
@@ -0,0 +1,15 @@
+package aero.minova.covid.test.print.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeststeckenController {
+	@GetMapping(value = "testStrecken/keyTexts", produces = "application/json")
+	public List<String> getTestStreckenKeyTexts() throws Exception {
+		return Arrays.asList("Eibelstadt", "Frickenhausen am Main", "Sommerhausen", "Winterhausen");
+	}
+}

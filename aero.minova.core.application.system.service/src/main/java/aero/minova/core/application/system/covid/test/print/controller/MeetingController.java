2ef14f7dae156e89a90341d5cce67e57d88e1b8c
avots <avots@minova.de>
2021-04-05 11:00:10 +0200

    Clearify name.

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/covid/test/print/controller/MeetingController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/covid/test/print/controller/MeetingController.java
@@ -4,24 +4,18 @@ import static java.util.Arrays.asList;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 
-import aero.minova.core.application.system.covid.test.print.repository.TestTerminRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import aero.minova.core.application.system.controller.SqlViewController;
 import aero.minova.core.application.system.covid.test.print.domain.DateRequest;
-import aero.minova.core.application.system.covid.test.print.domain.TimeLocation;
+import aero.minova.core.application.system.covid.test.print.domain.TimeLocationRequest;
 import aero.minova.core.application.system.covid.test.print.domain.Timeslot;
-import aero.minova.core.application.system.sql.SystemDatabase;
-import lombok.val;
 
 @CrossOrigin
 @RestController
@@ -30,7 +24,7 @@ public class MeetingController {
 	Logger logger = LoggerFactory.getLogger(MeetingController.class);
 
 	@PostMapping(value = "meeting/time/available", produces = "application/json")
-	public List<Timeslot> getAvailableTimeslot(@RequestBody TimeLocation timeLocation) throws Exception {
+	public List<Timeslot> getAvailableTimeslot(@RequestBody TimeLocationRequest timeLocationRequest) throws Exception {
 		return asList(new Timeslot(LocalDateTime.of(2021, 4, 1, 9, 0), LocalDateTime.of(2021, 4, 1, 9, 30)), //
 				new Timeslot(LocalDateTime.of(2021, 4, 1, 9, 30), LocalDateTime.of(2021, 4, 1, 10, 0)), //
 				new Timeslot(LocalDateTime.of(2021, 4, 1, 10, 0), LocalDateTime.of(2021, 4, 1, 10, 30)), //

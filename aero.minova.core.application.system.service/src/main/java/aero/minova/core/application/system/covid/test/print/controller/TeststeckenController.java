f74cf8b7f2385f107b08ff462d1e33fed616b4ff
avots <avots@minova.de>
2021-04-05 12:13:18 +0200

    #7 KeyLong für Teststrecke ausgeben.

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/covid/test/print/controller/TeststeckenController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/covid/test/print/controller/TeststeckenController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import aero.minova.core.application.system.controller.SqlProcedureController;
 import aero.minova.core.application.system.controller.SqlViewController;
+import aero.minova.core.application.system.covid.test.print.domain.TestStrecke;
 import aero.minova.core.application.system.domain.*;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,20 +24,21 @@ public class TeststeckenController {
     SqlViewController sqlViewController;
 
     @PostMapping(value = "testStrecken/keyTexts", produces = "application/json")
-    public List<String> getTestStreckenKeyTexts() throws Exception {
-        val param = new Table();
-        param.setName("xtctsTestStrecke");
-        param.addColumn(new Column("KeyText", DataType.STRING, OutputType.OUTPUT));
+    public List<TestStrecke> getTestStreckenKeyTexts() throws Exception {
+        val sqlRequest = new Table();
+        sqlRequest.setName("xtctsTestStrecke");
+        sqlRequest.addColumn(new Column("KeyLong", DataType.INTEGER, OutputType.OUTPUT));
+        sqlRequest.addColumn(new Column("KeyText", DataType.STRING, OutputType.OUTPUT));
 
         // Hiermit wird der unsichere Zugriff ermöglicht.
         val requestingAuthority = new Row();
         requestingAuthority.addValue(new Value(false, "1"));
         requestingAuthority.addValue(new Value(false, "2"));
         requestingAuthority.addValue(new Value(false, "3"));
-        return sqlViewController.unsecurelyGetIndexView (param, Arrays.asList(requestingAuthority))
+        return sqlViewController.unsecurelyGetIndexView (sqlRequest, Arrays.asList(requestingAuthority))
                 .getRows()
                 .stream()
-                .map(row -> row.getValues().get(0).getStringValue())
+                .map(row -> new TestStrecke(row.getValues().get(1).getStringValue(), row.getValues().get(0).getIntegerValue()))
                 .collect(toList());
     }
 }

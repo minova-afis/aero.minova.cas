6a94bc7a3754c710585a96a47918534012d40f95
Kerstin Weber <weber@minova.aero>
2021-04-12 12:42:41 +0200

    Login und finishRegistration für Testpersonen

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/covid/test/print/controller/TestPersonController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/covid/test/print/controller/TestPersonController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import aero.minova.core.application.system.controller.SqlProcedureController;
 import aero.minova.core.application.system.controller.SqlViewController;
 import aero.minova.core.application.system.covid.test.print.domain.TestPersonInformation;
+import aero.minova.core.application.system.covid.test.print.domain.UserInfo;
 import aero.minova.core.application.system.domain.Column;
 import aero.minova.core.application.system.domain.DataType;
 import aero.minova.core.application.system.domain.OutputType;
@@ -35,13 +36,40 @@ public class TestPersonController {
 	private final Pattern postalcodepattern = Pattern.compile("^0[1-9]\\d\\d(?<!0100)0|0[1-9]\\d\\d[1-9]|[1-9]\\d{3}[0-8]|[1-9]\\d{3}(?<!9999)9$");
 
 	@PostMapping(value = "testPerson/register", produces = "application/json")
-	public int registerTestPerson(@RequestBody TestPersonInformation input) throws Exception {
+	public void registerTestPerson(@RequestBody TestPersonInformation input) throws Exception {
 
 		checkUserInput(input);
 
 		// Überprüfen, ob die Person bereits im System registriert ist
 		Table sqlRequest = new Table();
 		sqlRequest.setName("xvctsTestPerson");
+		sqlRequest.addColumn(new Column("Email", DataType.STRING, OutputType.INPUT));
+		{
+			val firstRequestParams = new Row();
+			sqlRequest.getRows().add(firstRequestParams);
+			firstRequestParams.addValue(new Value(input.getEmail(), null));
+		}
+		// Hiermit wird der unsichere Zugriff ermöglicht.
+		val requestingAuthority = new Row();
+		requestingAuthority.addValue(new Value(false, "1"));
+		requestingAuthority.addValue(new Value(false, "2"));
+		requestingAuthority.addValue(new Value(false, "3"));
+
+		// Überprüfen, ob der Benutzer bereits exisitert, da keine Doppelteneiträge vorhanden sein sollten
+		List<Row> viewOutput = sqlViewController.unsecurelyGetIndexView(sqlRequest, Arrays.asList(requestingAuthority)).getRows();
+		if (!viewOutput.isEmpty()) {
+			throw new RuntimeException("Ein Benutzer mit dieser Emailadresse existiert bereits!");
+		}
+
+		// TODO Emailverifikation
+
+	}
+
+	@PostMapping(value = "testPerson/finishregistration", produces = "application/json")
+	public void finishRegistrationTestPerson(@RequestBody TestPersonInformation input) throws Exception {
+		// Anlegen des Benutzers
+		Table sqlRequest = new Table();
+		sqlRequest.setName("xpctsInsertTestPerson");
 		sqlRequest.addColumn(new Column("KeyLong", DataType.INTEGER, OutputType.OUTPUT));
 		sqlRequest.addColumn(new Column("FirstName", DataType.STRING, OutputType.INPUT));
 		sqlRequest.addColumn(new Column("LastName", DataType.STRING, OutputType.INPUT));
@@ -66,8 +94,7 @@ public class TestPersonController {
 			firstRequestParams.addValue(new Value(input.getPhonenumber(), null));
 			firstRequestParams.addValue(new Value(input.getPhonenumber2(), null));
 			firstRequestParams.addValue(new Value(input.getEmail(), null));
-			// wir prüfen nicht auf das Passwort!!!
-			firstRequestParams.addValue(null);
+			firstRequestParams.addValue(new Value(input.getPassword(), null));
 		}
 		// Hiermit wird der unsichere Zugriff ermöglicht.
 		val requestingAuthority = new Row();
@@ -75,26 +102,53 @@ public class TestPersonController {
 		requestingAuthority.addValue(new Value(false, "2"));
 		requestingAuthority.addValue(new Value(false, "3"));
 
-		// Überprüfen, ob der Benutzer bereits exisitert, da keine Doppelteneiträge vorhanden sein sollten
-		List<Row> viewOutput = sqlViewController.unsecurelyGetIndexView(sqlRequest, Arrays.asList(requestingAuthority)).getRows();
-		if (!viewOutput.isEmpty()) {
-			throw new RuntimeException("Dieser Benutzer existiert bereits!");
+		sqlProcedureController.calculateSqlProcedureResult(sqlRequest);
+
+	}
+
+	@PostMapping(value = "testPerson/login", produces = "application/json")
+	public long loginTestPerson(@RequestBody UserInfo info) throws Exception {
+
+		if (info.getEmail().isEmpty()) {
+			throw new RuntimeException("Bitte geben Sie Ihre Emailadresse ein.");
 		}
 
-		// TODO Emailverifikation
+		if (info.getPassword().isEmpty()) {
+			throw new RuntimeException("Bitte geben Sie Ihr Passwort ein.");
+		}
 
-		// Anlegen des Benutzers, dazu auch das Passwort hinzufügen
-		sqlRequest.setName("xpctsInsertTestPerson");
-		sqlRequest.getRows().get(0).getValues().get(10).setValue(input.getPassword());
-		int id = sqlProcedureController.calculateSqlProcedureResult(sqlRequest).getOutputParameters().getRows().get(0).getValues().get(0).getIntegerValue();
+		Table sqlRequest = new Table();
+		sqlRequest.setName("xvctsTestPerson");
+		sqlRequest.addColumn(new Column("KeyLong", DataType.INTEGER, OutputType.OUTPUT));
+		sqlRequest.addColumn(new Column("Email", DataType.STRING, OutputType.INPUT));
+		sqlRequest.addColumn(new Column("Password", DataType.STRING, OutputType.INPUT));
+		{
+			val firstRequestParams = new Row();
+			sqlRequest.getRows().add(firstRequestParams);
+			firstRequestParams.addValue(null);
+			firstRequestParams.addValue(new Value(info.getEmail(), null));
+			firstRequestParams.addValue(new Value(info.getPassword(), null));
+		}
+		// Hiermit wird der unsichere Zugriff ermöglicht.
+		val requestingAuthority = new Row();
+		requestingAuthority.addValue(new Value(false, "1"));
+		requestingAuthority.addValue(new Value(false, "2"));
+		requestingAuthority.addValue(new Value(false, "3"));
 
-		// Zurückgeben des KeyLongs
-		return id;
+		List<Row> result = sqlViewController.unsecurelyGetIndexView(sqlRequest, Arrays.asList(requestingAuthority)).getRows();
+
+		if (result.get(0).getValues().size() > 0) {
+			// Zurückgeben des KeyLongs
+			return Long.valueOf(result.get(0).getValues().get(0).getIntegerValue());
+		} else {
+			throw new RuntimeException("Fehler beim Login. Bitte überprüfen Sie Ihre angegebene Emailadresse und Ihr Passwort.");
+		}
+
 	}
 
 	private void checkUserInput(TestPersonInformation input) {
 
-		if (input.getFirstname() == null || input.getLastname() == null || input.getFirstname().length() <= 0 || input.getLastname().length() <= 0) {
+		if (input.getFirstname() == null || input.getLastname() == null || input.getFirstname().isEmpty() || input.getLastname().isEmpty()) {
 			throw new RuntimeException("Bitte geben Sie Ihren Vor- und Nachnamen an.");
 		} else if (input.getFirstname().length() > 50) {
 			throw new RuntimeException("Der angegebene Vorname übersteigt das Zeichenlimit!");
@@ -102,20 +156,20 @@ public class TestPersonController {
 			throw new RuntimeException("Der angegebene Nachname übersteigt das Zeichenlimit!");
 		}
 
-		if (input.getStreet() == null || input.getStreet().length() <= 0) {
+		if (input.getStreet() == null || input.getStreet().isEmpty()) {
 			throw new RuntimeException("Bitte geben Sie Ihre Adresse an.");
 		}
 		if (input.getStreet().length() > 50) {
 			throw new RuntimeException("Die angegebene Straße übersteigt das Zeichenlimit!");
 		}
 
-		if (input.getCity() == null || input.getCity().length() <= 0) {
+		if (input.getCity() == null || input.getCity().isEmpty()) {
 			throw new RuntimeException("Bitte geben Sie Ihren Wohnort an.");
 		} else if (input.getCity().length() > 20) {
 			throw new RuntimeException("Der angegebene Wohnort übersteigt das Zeichenlimit!");
 		}
 
-		if (input.getPostalcode() != null && input.getPostalcode().length() > 0) {
+		if (input.getPostalcode() != null && !input.getPostalcode().isEmpty()) {
 			Matcher matcher = postalcodepattern.matcher(input.getPostalcode());
 			if (!matcher.find()) {
 				throw new RuntimeException("Die angegebene Postleitzahl hat kein gültiges Format!");
@@ -132,7 +186,7 @@ public class TestPersonController {
 			throw new RuntimeException("Bitte geben Sie Ihren Geburtstag an.");
 		}
 
-		if (input.getPhonenumber() != null && input.getPhonenumber().length() > 0) {
+		if (input.getPhonenumber() != null && !input.getPhonenumber().isEmpty()) {
 			if (!input.getPhonenumber().matches("[0-9]+")) {
 				throw new RuntimeException("Die angegebene Telefonnummer ist nicht gültig!");
 			} else if (input.getPhonenumber().length() > 20) {
@@ -151,7 +205,7 @@ public class TestPersonController {
 			}
 		}
 
-		if (input.getEmail() != null && input.getEmail().length() > 0) {
+		if (input.getEmail() != null && !input.getEmail().isEmpty()) {
 			Matcher matcher = emailpattern.matcher(input.getEmail());
 			if (!matcher.find()) {
 				throw new RuntimeException("Die angegebene E-Mail Adresse hat kein gültiges Format!");
@@ -164,7 +218,7 @@ public class TestPersonController {
 			throw new RuntimeException("Bitte geben Sie Ihre E-Mailadresse an.");
 		}
 
-		if (input.getPassword() == null || input.getPassword().length() <= 0) {
+		if (input.getPassword() == null || input.getPassword().isEmpty()) {
 			throw new RuntimeException("Bitte geben Sie ein Passwort an.");
 		} else {
 			if (input.getPassword().length() <= 5) {

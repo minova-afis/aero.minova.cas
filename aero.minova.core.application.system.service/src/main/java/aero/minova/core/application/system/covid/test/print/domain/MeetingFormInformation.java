7e33a7ccd184113fd180fb58c065a928716d5f43
Kerstin Weber <weber@minova.aero>
2021-04-14 17:59:16 +0200

    Anpassen der Buchungsmethode

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/covid/test/print/domain/MeetingFormInformation.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/covid/test/print/domain/MeetingFormInformation.java
@@ -1,7 +1,5 @@
 package aero.minova.core.application.system.covid.test.print.domain;
 
-import java.time.LocalDate;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -11,6 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MeetingFormInformation {
 	private int testStreckKeyLong;
-	private LocalDate starttime;
+	private String starttime;
 	private long testPersonKeyLong;
 }

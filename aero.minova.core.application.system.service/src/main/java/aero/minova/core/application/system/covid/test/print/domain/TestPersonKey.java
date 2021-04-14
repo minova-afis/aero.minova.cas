8f8d40d21d573d3e3839779530302dc6f44622dd
Kerstin Weber <weber@minova.aero>
2021-04-14 17:30:24 +0200

    Testperson Information abfragen

---
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/covid/test/print/domain/TestPersonKey.java
@@ -0,0 +1,12 @@
+package aero.minova.core.application.system.covid.test.print.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+public class TestPersonKey {
+	private long CTSTestPersonKey;
+}

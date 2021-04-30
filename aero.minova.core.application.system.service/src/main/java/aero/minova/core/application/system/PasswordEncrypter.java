9690a98aaee429e005ee6de0ae5467f079894510
Kerstin Weber <weber@minova.aero>
2021-04-30 15:04:30 +0200

    Entfernen der CTS Inhalte

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/PasswordEncrypter.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/PasswordEncrypter.java
@@ -3,7 +3,7 @@ package aero.minova.core.application.system;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncrypter {
-    public static void main(String... args) {
-        System.out.println(new BCryptPasswordEncoder().encode("rqgzxTf71EAx8chvchMi"));
-    }
+	public static void main(String... args) {
+		System.out.println(new BCryptPasswordEncoder().encode("rqgzxTf71EAx8chvchMi"));
+	}
 }
--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/CommunicationController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/CommunicationController.java
@@ -1,12 +1,23 @@
 package aero.minova.core.application.system.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import aero.minova.core.application.system.domain.PingResponse;
 
 @RestController
 public class CommunicationController {
+
+	@Autowired
+	SqlProcedureController procedureController;
+
 	/**
 	 * Hiermit kann geprüft werden, ob die Kommunikation mit und die Anmeldung an den CAS funktioniert.
 	 * 
@@ -15,5 +26,16 @@ public class CommunicationController {
 	@GetMapping(value = "ping", produces = "application/json")
 	public PingResponse executePing() {
 		return new PingResponse();
+	}
+
+	@PostMapping(value = "loadPrivileges")
+	public void loadPrivileges() throws Exception {
+		// Abfrage mur für jUnit-Tests, da dabei Authentication = null
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication != null) {
+			procedureController.loadPrivileges(authentication.getName(), (List<GrantedAuthority>) authentication.getAuthorities());
+		} else {
+			throw new IllegalArgumentException("No User found, please login");
+		}
 	}
 }

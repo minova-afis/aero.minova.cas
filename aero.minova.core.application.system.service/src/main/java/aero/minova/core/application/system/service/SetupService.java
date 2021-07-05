a009d5452e4c3328bd8153933221a63443ce2bca
Kerstin Weber <weber@minova.aero>
2021-07-05 13:44:51 +0200

    aufgelöste Abhängigkeiten in Liste packen

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/service/SetupService.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/service/SetupService.java
@@ -1,13 +1,23 @@
 package aero.minova.core.application.system.service;
 
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Service;
 
 @Service
 public class SetupService {
+
 	public List<String> parseDependencyList(String arg) {
-		return new ArrayList<>();
+		List<String> dependencies = new ArrayList<>();
+		// Die obere Zeile und die die Zeilen mit den nicht resolvten Dateien abschneiden.
+		arg = arg.substring(arg.indexOf("\n") + 1);
+		arg = arg.substring(0, arg.indexOf("The following files have NOT been resolved:"));
+
+		// Am Zeilenumbruch trennen und störende Leerzeichen entfernen.
+		dependencies = Stream.of(arg.split("\n")).map(s -> s.strip()).collect(Collectors.toList());
+		return dependencies;
 	}
 }

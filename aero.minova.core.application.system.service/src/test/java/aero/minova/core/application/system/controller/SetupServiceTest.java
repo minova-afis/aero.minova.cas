63c8c72d2ae63c1c798ad67101ffd7d56ed7a6dd
Kerstin Weber <weber@minova.aero>
2021-07-06 13:39:44 +0200

    sql files auslesen und ausführen

--- aero.minova.core.application.system.service/src/test/java/aero/minova/core/application/system/controller/SetupServiceTest.java
+++ aero.minova.core.application.system.service/src/test/java/aero/minova/core/application/system/controller/SetupServiceTest.java
@@ -1,14 +1,28 @@
 package aero.minova.core.application.system.controller;
 
-import aero.minova.core.application.system.service.SetupService;
-import org.junit.jupiter.api.Test;
-
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import aero.minova.core.application.system.service.FilesService;
+import aero.minova.core.application.system.service.SetupService;
+import aero.minova.core.application.system.sql.SystemDatabase;
+
 public class SetupServiceTest {
+
+	@Mock
+	private SystemDatabase database;
+
+	@Mock
+	private FilesService service;
+
 	@Test
 	public void testExampleProject() {
 		InputStream inputStream = getClass().getClassLoader().getResourceAsStream("exampleDependencyList.txt");
@@ -17,5 +31,21 @@ public class SetupServiceTest {
 			text = scanner.useDelimiter("\\A").next();
 		}
 		new SetupService().parseDependencyList(text);
+	}
+
+	@Test
+	public void testReadSetups() throws Exception {
+		Path testPath = Paths.get(getClass().getClassLoader().getResource("Setup.xml").toURI());
+		Mockito.when(service.getSystemFolder()).thenReturn(testPath);
+
+		SetupService testSubject = new SetupService();
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream("exampleDependencyList.txt");
+		String text = null;
+		try (Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8.name())) {
+			text = scanner.useDelimiter("\\A").next();
+		}
+		System.out.println(text);
+		List<String> dependencies = testSubject.parseDependencyList(text);
+		testSubject.readSetups(dependencies);
 	}
 }

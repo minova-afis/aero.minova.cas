729d192f644363574a966dc88666943bb16abbdb
Kerstin Weber <weber@minova.aero>
2021-07-09 15:18:08 +0200

    Javadoc korrigieren

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/service/FilesService.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/service/FilesService.java
@@ -96,12 +96,16 @@ public class FilesService {
 	}
 
 	/**
-	 * This method populates all the files in a directory to a List
+	 * Diese Methode erzeugt eine Liste aller vorhandenen Files in einem Directory. Falls sich noch weitere Directories in diesem befinden, wird deren Inhalt
+	 * ebenfalls aufgelistet
 	 * 
 	 * @param dir
-	 * @throws IOException
+	 *            das zu durchsuchende Directory
+	 * @return eine Liste an allen Files in dem übergebenen Directory
+	 * @throws FileNotFoundException
+	 *             Falls das Directory nicht existiert oder der übergebene Pfad nicht auf ein Directory zeigt.
 	 */
-	public List<Path> populateFilesList(Path dir) throws IOException {
+	public List<Path> populateFilesList(Path dir) throws FileNotFoundException {
 		List<Path> filesListInDir = new ArrayList<>();
 		File[] files = dir.toFile().listFiles();
 		if (files == null) {

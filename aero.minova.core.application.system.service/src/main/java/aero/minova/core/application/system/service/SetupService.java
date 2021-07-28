805d387baf858b5ec0f180a777cb65b2c576e496
avots <avots@minova.de>
2021-07-28 17:35:00 +0200

    #157 Leerzeilen filtern und nicht selektieren.

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/service/SetupService.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/service/SetupService.java
@@ -80,7 +80,7 @@ public class SetupService {
 
 		// Am Zeilenumbruch trennen und störende Leerzeichen entfernen.
 		List<String> dependencies = Stream.of(arg.split("\\R"))//
-				.filter(s -> s.isEmpty())
+				.filter(s -> !s.isEmpty())
 				.map(s -> s.substring(0 + 1, s.indexOf(":jar")).replace(":", ".").strip())//
 				.collect(Collectors.toList());
 		Collections.reverse(dependencies);

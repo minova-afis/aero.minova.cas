0ec3d32572943cc2e1bd07d6e1ce3bd1223544cd
avots <avots@minova.de>
2021-08-18 12:02:44 +0200

    #157 Exception konsolodieren.

--- aero.minova.core.application.system.setup/src/main/java/aero/minova/core/application/system/setup/table/BaseSetupException.java
+++
@@ -1,8 +0,0 @@
-package aero.minova.core.application.system.setup.table;
-
-@SuppressWarnings("serial")
-public class BaseSetupException extends Exception {
-	public BaseSetupException(final String format) {
-		super(format);
-	}
-}

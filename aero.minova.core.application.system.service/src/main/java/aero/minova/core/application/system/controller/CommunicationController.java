2b31c8a8cbe0a898348d1f231a1e331e6a6f5754
weber-minova <weber@minova.aero>
2021-12-17 12:26:44 +0100

    #189 setup über Weboberfläche

--- aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/CommunicationController.java
+++ aero.minova.core.application.system.service/src/main/java/aero/minova/core/application/system/controller/CommunicationController.java
@@ -2,22 +2,34 @@ package aero.minova.core.application.system.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import aero.minova.core.application.system.domain.PingResponse;
+import aero.minova.core.application.system.domain.Table;
 import aero.minova.core.application.system.service.SecurityService;
 
 @RestController
 public class CommunicationController {
 
+	@Value("${server.servlet.context-path:}")
+	String homePath;
+
 	@Autowired
 	SecurityService securityService;
+
+	@Autowired
+	SqlProcedureController spc;
 
 	/**
 	 * Hiermit kann geprüft werden, ob die Kommunikation mit und die Anmeldung an den CAS funktioniert.
@@ -40,5 +52,25 @@ public class CommunicationController {
 		} catch (Exception e) {
 			throw new IllegalArgumentException("No User found, please login");
 		}
+	}
+
+	/**
+	 * Empfängt das Signal von der Web-Oberfläche und übersetzt es in die Setup-Table. Kehrt auf die Hauptseite zurück, wenn das Setup durchgelaufen ist.
+	 * 
+	 * @throws Exception
+	 *             Wenn beim Setup ein Fehler auftritt, zum Beispiel, wenn eine Prozedur fehlerhaft war.
+	 */
+	@RequestMapping(value = "/setup", method = RequestMethod.POST)
+	public void setup(HttpServletResponse httpServletResponse) throws Exception {
+		Table setupTable = new Table();
+		setupTable.setName("setup");
+		try {
+			spc.executeProcedure(setupTable);
+		} catch (Exception e) {
+			httpServletResponse.setHeader("Location", homePath + "/setupError");
+			httpServletResponse.setStatus(302);
+		}
+		httpServletResponse.setHeader("Location", homePath + "/setupSuccess");
+		httpServletResponse.setStatus(302);
 	}
 }

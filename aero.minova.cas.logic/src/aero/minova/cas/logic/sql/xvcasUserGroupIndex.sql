75ab77180d068e30a6ab7dabea283a5a73c6d33f
avots <avots@minova.de>
2022-03-03 12:59:44 +0100

    Nicht mehr verwendetes Projekt löschen.
    Das war der erste Entwurf des App-Projektes.

--- aero.minova.cas.logic/src/aero/minova/cas/logic/sql/xvcasUserGroupIndex.sql
+++
@@ -1,9 +0,0 @@
-alter view dbo.xvcasUserGroupIndex
-with encryption as
-	select	ug.KeyLong,
-			ug.KeyText,
-			ug.Description,
-			ug.UserCode,
-			ug.SecurityToken
-	from xtcasUserGroup ug
-	where ug.LastAction > 0

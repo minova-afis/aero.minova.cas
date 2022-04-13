e2e5885f28a7db38570ac895ed52d1587329ed5e
weber-minova <weber@minova.aero>
2022-04-13 14:23:13 +0200

    View anpassen

--- app/src/main/app/sql/xvcasCASServices.sql
+++ app/src/main/app/sql/xvcasCASServices.sql
@@ -1,5 +1,11 @@
 alter view dbo.xvcasCASServices
 AS
-select * from xtcasCASservices cs
+select
+cs.KeyText as ServiceName,
+cs.ServiceURL.
+cs.Port,
+nl.TableName,
+pn.KeyText as ProcedureName
+from xtcasCASservices cs
 left join xtcasNewsfeedListener nl on nl.CASServiceKey = cs.KeyLong
 left join xtcasProcedureNewsfeed pn on pn.TableName = nl. TableName

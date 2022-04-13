92783a213b772a98b1fbde8b416e696dad4e6aad
avots <avots@minova.de>
2022-04-13 09:15:44 +0200

    Typo fixen.

--- service/src/main/java/aero/minova/cas/sql/SystemDatabase.java
+++ service/src/main/java/aero/minova/cas/sql/SystemDatabase.java
@@ -27,7 +27,7 @@ public class SystemDatabase {
 	@Value("${aero_minova_database_user_password:password}")
 	String userPassword;
 
-	@Value("${aero.minova.database.kind:mysql}")
+	@Value("${aero.minova.database.kind:mssql}")
 	String databaseKind;
 
 	private LinkedList<Connection> freeConnections = new LinkedList<>();

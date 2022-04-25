0658e3745c63b6f799fc332367a41e0d5da02a2f
weber-minova <weber@minova.aero>
2022-04-25 07:44:25 +0200

    areServiceNotifiersStoresSetup Methode aus SecurityService entfernen

--- service/src/main/java/aero/minova/cas/service/SecurityService.java
+++ service/src/main/java/aero/minova/cas/service/SecurityService.java
@@ -51,18 +51,6 @@ public class SecurityService {
 		return isTablePresent("xvcasuserprivileges");
 	}
 
-	/**
-	 * Prüft, ob minimalen die Datenbank-Objekte notwendig für das Registrieren von Diensten und Newsfeeds vorhanden sind. Dazu prüft man, ob die
-	 * `xvcasCASServices` vorhanden ist.
-	 * 
-	 * @return True, falls xvcascasservices vorhanden ist.
-	 * @throws Exception
-	 *             Fehler bei der Emittlung
-	 */
-	public boolean areServiceNotifiersStoresSetup() throws Exception {
-		return isTablePresent("xvcascasservices");
-	}
-
 	private boolean isTablePresent(String tableName) throws Exception {
 		try (final Connection connection = systemDatabase.getConnection()) {
 			return connection.getMetaData()//

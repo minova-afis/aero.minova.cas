130887032231f320af3beb59868c3277b322a384
weber-minova <weber@minova.aero>
2022-04-26 15:41:13 +0200

    Löschen der Nachricht nach Alter und Versuchen hinzufügen

--- service/src/main/java/aero/minova/cas/service/QueueService.java
+++ service/src/main/java/aero/minova/cas/service/QueueService.java
@@ -1,5 +1,7 @@
 package aero.minova.cas.service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
@@ -29,6 +31,12 @@ public class QueueService implements BiConsumer {
 	@Autowired
 	SqlProcedureController spc;
 
+	@org.springframework.beans.factory.annotation.Value("${aero.minova.message.age:7}")
+	int allowedMessageAge;
+
+	@org.springframework.beans.factory.annotation.Value("${aero.minova.number.of.attempts:10}")
+	int allowedNumberOfAttempts;
+
 	@Override
 	public void accept(Object t, Object u) {
 		// TODO Generiere Nachricht und speichere sie in der xtcasServiceMessage
@@ -37,24 +45,73 @@ public class QueueService implements BiConsumer {
 
 	@Scheduled(cron = "${aero.minova.check.message.intervall:0 * * * * *}")
 	private void sendQueueMessage() {
+
+		// Holt sich alle Nachrichten, die noch nicht versandt wurden.
 		Table messagesToBeSend = getNextMessage();
+
 		if (messagesToBeSend != null && messagesToBeSend.getRows().size() > 0) {
 			List<Integer> blockedServices = new ArrayList<>();
 
-			for (Row pendingMessages : messagesToBeSend.getRows()) {
+			for (Row pendingMessage : messagesToBeSend.getRows()) {
 
-				boolean sendSuccessfull = sendMessage(messagesToBeSend);
+				// Falls die Nachricht älter ist als das allowedMessageAge und falls die NumberOfAttempts höher ist als die allowedNumberOfAttempts, muss die
+				// Nachricht gelöscht werden.
+				if (pendingMessage.getValues().get(8).getInstantValue().isBefore(Instant.now().minus(allowedMessageAge, ChronoUnit.DAYS))
+						&& pendingMessage.getValues().get(7).getIntegerValue() > allowedNumberOfAttempts) {
+					deleteMessage(pendingMessage);
+					continue;
+				}
+
+				// Versuche die Nachricht an den Dienst zu verschicken.
+				boolean sendSuccessfull = sendMessage(pendingMessage);
 				if (sendSuccessfull) {
-					safeAsSent(pendingMessages);
+					safeAsSent(pendingMessage);
 				} else {
 					/*
 					 * Falls eine Nachricht nicht an einen Dienst geschickt werden konnte, wird dessen Key auf die blockedServices-Liste gesetzt, da alle
 					 * weiteren Nachrichten von der nicht versandten anhängen könnten. Deshalb werden in diesem Intervall keine Nachrichten mehr an diesen
 					 * Dienst geschickt.
 					 */
-					blockedServices.add(pendingMessages.getValues().get(0).getIntegerValue());
+					blockedServices.add(pendingMessage.getValues().get(0).getIntegerValue());
+					increaseAttempts(pendingMessage);
 				}
 			}
+		}
+	}
+
+	private void deleteMessage(Row pendingMessage) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Erhöht die NumberOfAttempts der übergebenen Nachricht.
+	 * 
+	 * @param pendingMessages
+	 *            Eine Row mit den Übergabeparamtern:CASServiceKey, CASServiceName, ServiceURL, Port, MessageKey, Message, isSent, NumberOfAttempts,
+	 *            MessageCreationDate
+	 */
+	private void increaseAttempts(Row pendingMessages) {
+		int attempts = pendingMessages.getValues().get(7).getIntegerValue() + 1;
+
+		Table setSent = new Table();
+		setSent.setName("xpcasUpdateServiceMessage");
+		setSent.addColumn(new Column("KeyLong", DataType.INTEGER));
+		setSent.addColumn(new Column("isSent", DataType.BOOLEAN));
+		setSent.addColumn(new Column("NumberOfAttempts", DataType.INTEGER));
+
+		Row setSentRow = new Row();
+		setSentRow.addValue(pendingMessages.getValues().get(4));
+		setSentRow.addValue(pendingMessages.getValues().get(6));
+		setSentRow.addValue(new Value(attempts, null));
+
+		setSent.addRow(setSentRow);
+		try {
+			spc.unsecurelyProcessProcedure(setSent);
+		} catch (Exception e) {
+			logger.logError("The message with key " + pendingMessages.getValues().get(4).getStringValue()
+					+ " could not be send! Number of attempts is increased to " + attempts, e);
+			throw new RuntimeException(e);
 		}
 	}
 
@@ -71,8 +128,10 @@ public class QueueService implements BiConsumer {
 		messagesRequest.addColumn(new Column("Message", DataType.STRING));
 		messagesRequest.addColumn(new Column("isSent", DataType.BOOLEAN));
 		messagesRequest.addColumn(new Column("NumberOfAttempts", DataType.INTEGER));
+		messagesRequest.addColumn(new Column("MessageCreationDate", DataType.INSTANT));
 
 		Row messagesRow = new Row();
+		messagesRow.addValue(null);
 		messagesRow.addValue(null);
 		messagesRow.addValue(null);
 		messagesRow.addValue(null);
@@ -86,15 +145,14 @@ public class QueueService implements BiConsumer {
 		try {
 			unsendMessages = securityService.unsecurelyGetIndexView(messagesRequest);
 		} catch (Exception e) {
-			logger.logError("The QueueService could not access view xvcasCASServiceMessage", e);
+			logger.logError("The QueueService could not access the view xvcasCASServiceMessage", e);
 			throw new RuntimeException(e);
 		}
 		return unsendMessages;
 	}
 
-	private boolean sendMessage(Table nextMessage) {
-		// TODO Falls NumberOfAttempts zu hoch -> Nachricht 'löschen'
-		// TODO Zu alte Nachrichten löschen (7 Tage?)
+	private boolean sendMessage(Row nextMessage) {
+
 		// TODO Nachrichten versenden
 		return false;
 	}
@@ -102,24 +160,14 @@ public class QueueService implements BiConsumer {
 	private void safeAsSent(Row nextMessage) {
 		Table setSent = new Table();
 		setSent.setName("xpcasUpdateServiceMessage");
-		setSent.addColumn(new Column("CASServiceKey", DataType.INTEGER));
-		setSent.addColumn(new Column("CASServiceName", DataType.STRING));
-		setSent.addColumn(new Column("ServiceURL", DataType.STRING));
-		setSent.addColumn(new Column("Port", DataType.INTEGER));
-		setSent.addColumn(new Column("MessageKey", DataType.INTEGER));
-		setSent.addColumn(new Column("Message", DataType.STRING));
+		setSent.addColumn(new Column("KeyLong", DataType.INTEGER));
 		setSent.addColumn(new Column("isSent", DataType.BOOLEAN));
 		setSent.addColumn(new Column("NumberOfAttempts", DataType.INTEGER));
 
 		Row setSentRow = new Row();
-		setSentRow.addValue(null);
-		setSentRow.addValue(null);
-		setSentRow.addValue(null);
-		setSentRow.addValue(null);
 		setSentRow.addValue(nextMessage.getValues().get(4));
-		setSentRow.addValue(null);
 		setSentRow.addValue(new Value(true, null));
-		setSentRow.addValue(null);
+		setSentRow.addValue(nextMessage.getValues().get(7));
 
 		setSent.addRow(setSentRow);
 		try {

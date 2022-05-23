ecc10b58e58f3ea0ac3ebb200a7dcd60dec3143b
scholz-afis <scholz@minova.aero>
2022-05-23 09:13:48 +0200

    scr: bugfix

--- service/src/main/java/aero/minova/cas/CoreApplicationSystemApplication.java
+++ service/src/main/java/aero/minova/cas/CoreApplicationSystemApplication.java
@@ -6,13 +6,13 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-// Der ComponentScan wird verwendet, damit alle Minova Komponenten im classpath geladen werden.
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @ComponentScan({ "aero.minova", "com.minova" })
 @EntityScan("aero.minova")
 @EnableJpaRepositories("aero.minova")

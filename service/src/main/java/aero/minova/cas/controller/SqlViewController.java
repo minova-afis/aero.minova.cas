3a7225c4f4693c2b88db85bf11304152c420edd2
weber-minova <weber@minova.aero>
2022-07-06 13:14:13 +0200

    JavaDoc hinzufügen

--- service/src/main/java/aero/minova/cas/controller/SqlViewController.java
+++ service/src/main/java/aero/minova/cas/controller/SqlViewController.java
@@ -358,7 +358,8 @@ public class SqlViewController {
 	}
 
 	/*
-	 * Pagination nach der Seek-Methode; bessere Performance als Offset bei großen Datensätzen
+	 * Pagination nach der Seek-Methode; bessere Performance als Offset bei großen Datensätzen. Wird NICHT für den "normalen" Index-Aufruf verwendet, da immer
+	 * davon ausgegangen wird, dass ein KeyLong in der View/Table vorhanden ist.
 	 */
 	public String pagingWithSeek(Table params, boolean autoLike, int maxRows, boolean count, int page, List<Row> authorities) {
 		final StringBuffer sb = new StringBuffer();

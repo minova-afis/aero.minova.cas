869e925f14cd2037eb022521fc968e0cf3a07871
weber-minova <weber@minova.aero>
2022-08-11 10:01:22 +0200

    Issue #184 Tests, die zeigen, dass Property funktioniert

---
+++ service/src/test/java/aero/minova/cas/HTTPTest.java
@@ -0,0 +1,30 @@
+package aero.minova.cas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class HTTPTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Test
+	public void http1Test() throws IOException, InterruptedException {
+
+		java.net.http.HttpClient client = java.net.http.HttpClient.newBuilder().build();
+		java.net.http.HttpRequest request = java.net.http.HttpRequest.newBuilder().uri(URI.create("http://localhost:" + port + "/ping")).build();
+		java.net.http.HttpResponse<String> response = client.send(request, java.net.http.HttpResponse.BodyHandlers.ofString());
+
+		assertNotNull(response);
+		assertEquals(java.net.http.HttpClient.Version.HTTP_1_1, response.version());
+	}
+}

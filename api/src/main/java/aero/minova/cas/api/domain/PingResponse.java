6af29b8bf5cc51caaf482f5405b54cc032c52e12
weber-minova <weber@minova.aero>
2022-08-12 15:48:38 +0200

    Variable static setzen

--- api/src/main/java/aero/minova/cas/api/domain/PingResponse.java
+++ api/src/main/java/aero/minova/cas/api/domain/PingResponse.java
@@ -1,7 +1,7 @@
 package aero.minova.cas.api.domain;
 
 public class PingResponse {
-	private final Integer returnCode = 0;
+	private static final Integer returnCode = 0;
 
 	public Integer getReturnCode() {
 		return returnCode;

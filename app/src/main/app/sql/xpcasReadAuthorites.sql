e442ee5ce26e4f8ec601210d1405312a423dcac0
weber-minova <weber@minova.aero>
2022-09-22 12:41:30 +0200

    Masken und Read-Prozeduren korrigieren

--- app/src/main/app/sql/xpcasReadAuthorites.sql
+++ app/src/main/app/sql/xpcasReadAuthorites.sql
@@ -9,5 +9,7 @@ with encryption as
 		from xtcasAuthorities a
 		left join xtcasUserGroup ug on a.Authority=ug.KeyText
 	where Username = @Username
+	and a.LastAction > 0
+	and ug.LastAction > 0
 
 return @@error

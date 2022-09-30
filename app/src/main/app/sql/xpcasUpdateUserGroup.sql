a33961f93f064e9b8e181aaf421ad77ebe2d4f23
weber-minova <weber@minova.aero>
2022-09-30 16:19:30 +0200

    Beim Ändern eines UserCodes müssen auch die Memberships angepasst werden

--- app/src/main/app/sql/xpcasUpdateUserGroup.sql
+++ app/src/main/app/sql/xpcasUpdateUserGroup.sql
@@ -6,6 +6,7 @@ alter procedure dbo.xpcasUpdateUserGroup (
 	@SecurityToken nvarchar(250) = null
 )
 with encryption as
+
 	if (exists(select * from xtcasUserGroup
 		where KeyText = @KeyText
 		  and KeyLong <> @KeyLong
@@ -15,10 +16,54 @@ with encryption as
 		return -1
 	end
 
+	declare @UserCodeOld nvarchar(50)
+
+	select @UserCodeOld = UserCode from xtcasUserGroup where KeyLong=@KeyLong
+
+
 	update xtcasUserGroup
 	set KeyText = @KeyText,
 		Description = @Description,
 		UserCode = @UserCode,
 		SecurityToken = @SecurityToken
 	where KeyLong = @KeyLong
+	
+
+	if (@UserCodeOld<>@UserCode)
+	begin
+
+		declare @Memberships nvarchar(max),
+		@UserKey int
+		
+
+		declare user_cursor cursor for 
+		select KeyLong, Memberships 
+		from xtcasUser
+
+		open user_cursor
+		fetch next from user_cursor into @UserKey, @Memberships 
+	
+		while @@FETCH_STATUS = 0 
+		begin
+
+			declare @UserGroupIsInMembership int
+
+			-- Überprüfen, ob es innerhalb der Memberships eine Membership der UserGroup gibt.
+			select @Memberships=Memberships from xtcasUser where KeyLong=@UserKey and LastAction > 0
+			select @UserGroupIsInMembership = CHARINDEX(@UserCodeOld, @Memberships) 
+
+			if (@UserGroupIsInMembership <> 0)
+			begin 
+				exec xpcasDeleteUserGroupUser @KeyLong,@UserKey
+				exec xpcasInsertUserGroupUser @KeyLong,@UserKey
+			end
+		
+			fetch next from user_cursor into @UserKey, @Memberships 
+		end
+
+		close user_cursor 
+		deallocate user_cursor 
+	end
+
+
 return @@error

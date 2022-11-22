42e941967b27332a013d3a66d1f2596cb489041c
David Bauer <bauer@minova.de>
2022-11-22 12:29:15 +0100

    MdiTypeKey 3 nur einemalig zulassen

--- app/src/main/app/sql/xpcasInitMdi.sql
+++ app/src/main/app/sql/xpcasInitMdi.sql
@@ -9,30 +9,42 @@ alter procedure dbo.xpcasInitMdi (
 )
 with encryption
 as
-
+	declare @Execute bit = 1
+	
+	if(@MdiTypeKey = 3)
+	begin
+		if exists(select * from xtcasMdi where LastAction > 0 and MdiTypeKey = 3)
+		begin
+			set @Execute = 0
+		end
+	end 
+	
 	if not exists (select * from xtcasMdi where LastAction >-1 and ID = @ID)
 	begin
-		insert into xtcasMdi(
-			ID,
-			Icon,
-			Label,
-			Menu,
-			Position,
-			SecurityToken,
-			MdiTypeKey,
-			LastUser,
-			LastDate,
-			LastAction
-		) values (
-			@ID,
-			@Icon,
-			@Label,
-			@Menu,
-			@Position,
-			@SecurityToken,
-			@MdiTypeKey,
-			dbo.xfCasUser(),
-			getDate(),
-			1
-		)
+		if (@Execute = 1)
+		begin
+			insert into xtcasMdi(
+				ID,
+				Icon,
+				Label,
+				Menu,
+				Position,
+				SecurityToken,
+				MdiTypeKey,
+				LastUser,
+				LastDate,
+				LastAction
+			) values (
+				@ID,
+				@Icon,
+				@Label,
+				@Menu,
+				@Position,
+				@SecurityToken,
+				@MdiTypeKey,
+				dbo.xfCasUser(),
+				getDate(),
+				1
+			)
+		end
 	end

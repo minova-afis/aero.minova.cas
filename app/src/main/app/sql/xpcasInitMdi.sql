eee213ec4f6a7104326bbc610abb2a7c80e802bb
Lea Janiak <janiak@minova.aero>
2022-11-22 14:21:22 +0100

    Neue Spalte in Mdi Tabelle, ModulName

--- app/src/main/app/sql/xpcasInitMdi.sql
+++ app/src/main/app/sql/xpcasInitMdi.sql
@@ -5,7 +5,8 @@ alter procedure dbo.xpcasInitMdi (
 	@Menu nvarchar(100),
 	@Position float,
 	@SecurityToken nvarchar(50),
-	@MdiTypeKey int
+	@MdiTypeKey int,
+	@ModulName nvarchar(500)
 )
 with encryption
 as
@@ -16,12 +17,25 @@ as
 		if exists(select * from xtcasMdi where LastAction > 0 and MdiTypeKey = 3)
 		begin
 			set @Execute = 0
+
+			update xtcasMdi set 
+				Icon = @Icon,
+				Label = @Label,
+				Menu = @Menu,
+				Position = @Position,
+				SecurityToken = @SecurityToken,
+				MdiTypeKey = @MdiTypeKey,
+				ModulName = @ModulName,
+				LastUser = dbo.xfCasUser(),
+				LastDate = getDate(),
+				LastAction = 2
+			where MdiTypeKey = 3 and LastAction > 0
 		end
 	end 
 	
-	if not exists (select * from xtcasMdi where LastAction >-1 and ID = @ID)
-	begin
-		if (@Execute = 1)
+	if (@Execute = 1)
+		begin
+		if not exists (select * from xtcasMdi where LastAction >-1 and ID = @ID)
 		begin
 			insert into xtcasMdi(
 				ID,
@@ -31,6 +45,7 @@ as
 				Position,
 				SecurityToken,
 				MdiTypeKey,
+				ModulName,
 				LastUser,
 				LastDate,
 				LastAction
@@ -42,9 +57,25 @@ as
 				@Position,
 				@SecurityToken,
 				@MdiTypeKey,
+				@ModulName,
 				dbo.xfCasUser(),
 				getDate(),
 				1
 			)
 		end
+		else 
+		begin 
+			update xtcasMdi set 
+					Icon = @Icon,
+					Label = @Label,
+					Menu = @Menu,
+					Position = @Position,
+					SecurityToken = @SecurityToken,
+					MdiTypeKey = @MdiTypeKey,
+					ModulName = @ModulName,
+					LastUser = dbo.xfCasUser(),
+					LastDate = getDate(),
+					LastAction = 2
+				where ID = @Id and LastAction > 0
+		end 
 	end

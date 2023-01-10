6488e307d46ccdf4c2b6ab32cbbc1bf079eed620
scholz-afis <scholz@minova.aero>
2023-01-10 11:49:44 +0100

    scr: fixes after 1st review https://github.com/minova-afis/aero.minova.cas/issues/403

--- service/src/main/java/aero/minova/cas/CoreApplicationSystemApplication.java
+++ service/src/main/java/aero/minova/cas/CoreApplicationSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 

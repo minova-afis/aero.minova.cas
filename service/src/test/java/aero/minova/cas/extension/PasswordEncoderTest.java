4e67e448c03971e71240571b2e7432aa24788f73
weber-minova <weber@minova.aero>
2023-04-17 15:48:46 +0200

    PasswordEncoder Extension komplett abdecken

--- service/src/test/java/aero/minova/cas/extension/PasswordEncoderTest.java
+++ service/src/test/java/aero/minova/cas/extension/PasswordEncoderTest.java
@@ -1,12 +1,15 @@
 package aero.minova.cas.extension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import aero.minova.cas.CoreApplicationSystemApplication;
+import aero.minova.cas.api.domain.DataType;
 import aero.minova.cas.api.domain.Row;
 import aero.minova.cas.api.domain.SqlProcedureResult;
 import aero.minova.cas.api.domain.Table;
@@ -52,6 +56,7 @@ public class PasswordEncoderTest extends BaseTest {
 
 	}
 
+	@DisplayName("Teste erfolgreiche Verschlüsselung.")
 	@Test
 	@WithMockUser(username = "admin", roles = { "ADMIN" })
 	public void testEncodePassword() throws Exception {
@@ -60,6 +65,58 @@ public class PasswordEncoderTest extends BaseTest {
 		SqlProcedureResult result = (SqlProcedureResult) spc.executeProcedure(test).getBody();
 
 		assertTrue(passwordEncoder.matches("TEST", result.getResultSet().getRows().get(0).getValues().get(1).getStringValue()));
+
+	}
+
+	@DisplayName("Password-Spalte fehlt.")
+	@Test
+	@WithMockUser(username = "admin", roles = { "ADMIN" })
+	public void testEncodePasswordMissingColumnPassword() throws Exception {
+		Table test = readTableFromExampleJson("jsons/PasswordTest2");
+
+		Throwable exception = assertThrows(aero.minova.cas.api.domain.ProcedureException.class, () -> spc.executeProcedure(test));
+		assertThat(exception).hasMessage("java.lang.IllegalArgumentException: Column `Password` is missing.");
+
+	}
+
+	@DisplayName("Encoded-Password-Spalte fehlt.")
+	@Test
+	@WithMockUser(username = "admin", roles = { "ADMIN" })
+	public void testEncodePasswordMissingColumnEncodedPassword() throws Exception {
+		Table test = readTableFromExampleJson("jsons/PasswordTest3");
+
+		Throwable exception = assertThrows(aero.minova.cas.api.domain.ProcedureException.class, () -> spc.executeProcedure(test));
+		assertThat(exception).hasMessage("java.lang.IllegalArgumentException: Column `EncodedPassword` is missing.");
+
+	}
+
+	@DisplayName("Inhalt/Value der Password-Spalte fehlt.")
+	@Test
+	@WithMockUser(username = "admin", roles = { "ADMIN" })
+	public void testEncodePasswordMissingPassword() throws Exception {
+		Table test = readTableFromExampleJson("jsons/PasswordTest4");
+
+		Throwable exception = assertThrows(aero.minova.cas.api.domain.ProcedureException.class, () -> spc.executeProcedure(test));
+		assertThat(exception).hasMessage("java.lang.IllegalArgumentException: Column `Password` has no value.");
+
+	}
+
+	@DisplayName("Falsche Spaltentypen.")
+	@Test
+	@WithMockUser(username = "admin", roles = { "ADMIN" })
+	public void testEncodePasswordWrongColumnTypes() throws Exception {
+		Table test = readTableFromExampleJson("jsons/PasswordTest");
+
+		test.getColumns().get(0).setType(DataType.BOOLEAN);
+		test.getColumns().get(1).setType(DataType.BOOLEAN);
+
+		Throwable exception = assertThrows(aero.minova.cas.api.domain.ProcedureException.class, () -> spc.executeProcedure(test));
+		assertThat(exception).hasMessage("java.lang.IllegalArgumentException: Column `Password` has to be of type String but is `BOOLEAN`.");
+
+		test.getColumns().get(0).setType(DataType.STRING);
+
+		Throwable exception2 = assertThrows(aero.minova.cas.api.domain.ProcedureException.class, () -> spc.executeProcedure(test));
+		assertThat(exception2).hasMessage("java.lang.IllegalArgumentException: Column `EncodedPassword` has to be of type String but is `BOOLEAN`.");
 
 	}
 

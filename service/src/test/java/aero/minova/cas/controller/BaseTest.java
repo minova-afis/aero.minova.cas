c85ff43b448985b14fb33c90f39f30941148d687
weber-minova <weber@minova.aero>
2023-04-17 15:07:15 +0200

    BaseTest um Hilfsmethode erweitern

--- service/src/test/java/aero/minova/cas/controller/BaseTest.java
+++ service/src/test/java/aero/minova/cas/controller/BaseTest.java
@@ -5,12 +5,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 import org.junit.AfterClass;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.google.gson.Gson;
+
+import aero.minova.cas.api.domain.Table;
+import aero.minova.cas.api.restapi.ClientRestAPI;
 import aero.minova.cas.service.FilesService;
 
 public abstract class BaseTest {
+
+	Gson gson;
+
+	@Autowired
+	ClientRestAPI clientRestAPI;
 
 	@AfterClass
 	protected static void cleanInternalFolder() throws Exception {
@@ -30,4 +41,17 @@ public abstract class BaseTest {
 			internalFolder.toFile().delete();
 		}
 	}
+
+	@SuppressWarnings("resource")
+	public Table readTableFromExampleJson(String fileName) {
+
+		gson = clientRestAPI.getGson();
+		return gson.fromJson(new Scanner(getClass()//
+				.getClassLoader()//
+				.getResourceAsStream(fileName + ".json"), "UTF-8")//
+						.useDelimiter("\\A")//
+						.next(),
+				Table.class);
+	}
+
 }

989bebf3eca5f9919f80d887e3e21e76b8605369
Bauer-David <61147549+Bauer-David@users.noreply.github.com>
2023-05-10 09:26:54 +0200

    Eingabe von längeren Namen im KeyText Feld von User ermoeglichen (#545)
    * Eingabe von längeren Namen im KeyText Feld von User ermöglichen
    
    * Update CHANGELOG.adoc

--- app/src/main/app/sql/xpcasInsertUser.sql
+++ app/src/main/app/sql/xpcasInsertUser.sql
@@ -1,6 +1,6 @@
 alter procedure dbo.xpcasInsertUser (
 	@KeyLong int output,
-	@KeyText nvarchar(10) = null,
+	@KeyText nvarchar(50) = null,
 	@UserSecurityToken nvarchar(50) = null,
 	@Memberships nvarchar(250) = null
 )

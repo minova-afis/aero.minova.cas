77a46a9e969f30e8413895911c10c2cc4e2452fe
scholz-afis <scholz@minova.aero>
2023-05-10 17:29:29 +0200

    scr: refactored CommunicationController to OpenAPI https://github.com/minova-afis/aero.minova.cas/issues/349

--- service/src/main/java/aero/minova/cas/controller/CommunicationController.java
+++ service/src/main/java/aero/minova/cas/controller/CommunicationController.java
@@ -1,78 +1,73 @@
 package aero.minova.cas.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
+import cas.openapi.api.server.api.LoadPrivilegesApi;
+import cas.openapi.api.server.api.PingApi;
+import cas.openapi.api.server.api.SetupApi;
+import cas.openapi.api.server.model.PingEntry;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import aero.minova.cas.CustomLogger;
-import aero.minova.cas.api.domain.PingResponse;
 import aero.minova.cas.api.domain.Table;
 import aero.minova.cas.service.SecurityService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
-public class CommunicationController {
-
-	@Value("${server.servlet.context-path:}")
-	String homePath;
-
-	@Autowired
-	SqlProcedureController spc;
-
-	@Autowired
-	SecurityService securityService;
-
-	@Autowired
-	CustomLogger customLogger;
+@RequiredArgsConstructor
+public class CommunicationController implements PingApi, LoadPrivilegesApi, SetupApi {
+	private final SqlProcedureController spc;
+	private final SecurityService securityService;
+	private final CustomLogger customLogger;
 
 	/**
 	 * Hiermit kann geprüft werden, ob die Kommunikation mit und die Anmeldung an den CAS funktioniert.
-	 * 
+	 *
 	 * @return PingResponse Diese Antwort signalisiert, dass es funktioniert hat.
 	 */
-	@GetMapping(value = "ping", produces = "application/json")
-	public PingResponse executePing() {
-		return new PingResponse();
+	@Override
+	public ResponseEntity<PingEntry> ping() {
+		return ResponseEntity.ok(new PingEntry().returnCode(0));
 	}
 
-	@PostMapping(value = "loadPrivileges")
-	public void loadPrivileges() throws Exception {
-
+	@Override
+	public ResponseEntity<Void> loadPrivileges() {
 		try {
 			securityService.loadAllPrivileges();
+			return ResponseEntity.noContent().build();
 		} catch (Exception e) {
-			customLogger.logError("Erorr while trying to load privileges!", e);
+			customLogger.logError("Error while trying to load privileges!", e);
 			throw new RuntimeException(e);
 		}
 	}
 
 	/**
 	 * Empfängt das Signal von der Web-Oberfläche und übersetzt es in die Setup-Table. Kehrt auf die Hauptseite zurück, wenn das Setup durchgelaufen ist.
-	 * 
-	 * @throws Exception
-	 *             Wenn beim Setup ein Fehler auftritt, zum Beispiel, wenn eine Prozedur fehlerhaft war.
 	 */
-	@RequestMapping(value = "/setup", method = RequestMethod.POST)
-	public void setup(HttpServletResponse httpServletResponse) throws Exception {
+	@Override
+	public ResponseEntity<Void> setup() {
 		Table setupTable = new Table();
 		setupTable.setName("setup");
-		boolean success = true;
+
 		try {
 			spc.executeProcedure(setupTable);
+			return ResponseEntity.status(HttpStatus.FOUND).location(generateResourceUriWithLastSegmentReplacedSuffix("setupSuccess")).build();
 		} catch (Exception e) {
-			success = false;
+			return ResponseEntity.status(HttpStatus.FOUND).location(generateResourceUriWithLastSegmentReplacedSuffix("setupError")).build();
 		}
-		if (success) {
-			httpServletResponse.setHeader("Location", homePath + "/setupSuccess");
-			httpServletResponse.setStatus(302);
-		} else {
-			httpServletResponse.setHeader("Location", homePath + "/setupError");
-			httpServletResponse.setStatus(302);
-		}
+	}
+
+	private URI generateResourceUriWithLastSegmentReplacedSuffix(String pathSegment) {
+		return ServletUriComponentsBuilder //
+				.fromCurrentRequest() //
+				.replaceQuery("") //
+				.path("/../{pathSegment}") //
+				.buildAndExpand(pathSegment) //
+				.normalize() //
+				.toUri();
 	}
 }

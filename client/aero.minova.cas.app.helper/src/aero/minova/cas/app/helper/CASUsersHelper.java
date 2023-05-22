aa9bdec059e792ab3b41b2303c6113e5be55cb18
Lea Janiak <77741125+janiak-minova@users.noreply.github.com>
2023-05-22 11:58:46 +0200

    PasswordEncoderExtension entfernen, Passwort schon im Helper verschlüsseln (#558)
    * Java 17, Smells und Warnungen aufräumen
    
    * Helper baubar machen
    
    * PasswordEncoderExtension aus dem CAS entfernen
    
    Ich halte es für eine sehr schlechte Idee, Passwörter in Klartext ans
    CAS zu schicken, um sie dort verschlüsseln zu lassen.
    
    * Passwort schon im Helper verschlüsseln
    
    * Update Changelog.md
    
    ---------
    
    Co-authored-by: Rainer Scholz <95353437+scholz-afis@users.noreply.github.com>

--- client/aero.minova.cas.app.helper/src/aero/minova/cas/app/helper/CASUsersHelper.java
+++ client/aero.minova.cas.app.helper/src/aero/minova/cas/app/helper/CASUsersHelper.java
@@ -1,28 +1,16 @@
 package aero.minova.cas.app.helper;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
 import javax.inject.Inject;
 
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.services.events.IEventBroker;
-import org.eclipse.e4.core.services.log.Logger;
-import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.osgi.service.component.annotations.Component;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import aero.minova.rcp.constants.Constants;
 import aero.minova.rcp.dataservice.IDataService;
-import aero.minova.rcp.model.DataType;
-import aero.minova.rcp.model.OutputType;
-import aero.minova.rcp.model.Row;
-import aero.minova.rcp.model.SqlProcedureResult;
-import aero.minova.rcp.model.Table;
-import aero.minova.rcp.model.TransactionEntry;
-import aero.minova.rcp.model.TransactionResultEntry;
 import aero.minova.rcp.model.Value;
-import aero.minova.rcp.model.builder.RowBuilder;
-import aero.minova.rcp.model.builder.TableBuilder;
 import aero.minova.rcp.model.form.MDetail;
 import aero.minova.rcp.model.form.MField;
 import aero.minova.rcp.model.helper.ActionCode;
@@ -33,13 +21,9 @@ import aero.minova.rcp.rcp.util.WFCDetailCASRequestsUtil;
 public class CASUsersHelper implements IHelper {
 
 	@Inject
-	MPerspective mPerspective;
-
-	@Inject
 	IDataService dataService;
 
-	@Inject
-	Logger logger;
+	ILog logger = Platform.getLog(this.getClass());
 
 	@Inject
 	IEventBroker eventBroker;
@@ -70,42 +54,11 @@ public class CASUsersHelper implements IHelper {
 			password.setRequired(true);
 			break;
 		}
-
 	}
 
 	private void encryptPassword(Value stringValue) {
-		Table requestTable = TableBuilder.newTable("xpcasEncodePassword") //
-				.withColumn("Password", DataType.STRING, OutputType.INPUT)//
-				.withColumn("EncodedPassword", DataType.STRING, OutputType.OUTPUT)//
-				.create();
-		Row row = RowBuilder.newRow() //
-				.withValue(stringValue) //
-				.withValue(null)//
-				.create();
-		requestTable.addRow(row);
-
-		List<TransactionEntry> procedureList = new ArrayList<>();
-		procedureList.add(new TransactionEntry(Constants.TRANSACTION_PARENT, requestTable));
-		CompletableFuture<List<TransactionResultEntry>> callTransactionAsync = dataService.callTransactionAsync(procedureList);
-
-		try {
-			SqlProcedureResult results = callTransactionAsync.get().get(0).getSQLProcedureResult(); // Warten bis Aufruf fertig ist
-			if (results != null && results.getResultSet() != null) {
-				Row resultSet = results.getResultSet().getRows().get(0);
-				if (resultSet.getValue(1) != null) {
-					password.setValue(new Value(resultSet.getValue(1).getStringValue()), false);
-					return;
-				}
-			}
-
-			// Falls das Passwort nicht verschlüsselt werden konnte, Fehler melden.
-			eventBroker.send(Constants.BROKER_SHOWERRORMESSAGE,
-					"Passwort konnte nicht verschlüsselt werden. Bitte versuchen Sie es erneut oder mit einem anderen Passwort.");
-
-		} catch (Exception e) {
-			logger.error(e);
-			eventBroker.send(Constants.BROKER_SHOWERRORMESSAGE, e.getMessage()); // Fehlermeldung
-		}
+		String encoded = new BCryptPasswordEncoder().encode(stringValue.getStringValue());
+		password.setValue(new Value(encoded), false);
 	}
 
 }

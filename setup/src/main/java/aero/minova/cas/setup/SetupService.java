7db26b1b805a0de9a2d7210619de00fd90a1eb90
David Bauer <bauer@minova.de>
2023-06-01 16:04:16 +0200

    Abschneiden von Daten im Setup erlauben

--- setup/src/main/java/aero/minova/cas/setup/SetupService.java
+++ setup/src/main/java/aero/minova/cas/setup/SetupService.java
@@ -63,6 +63,7 @@ public class SetupService {
 		spc.registerExtension(PROCEDURE_NAME, inputTable -> {
 			try {
 				SqlProcedureResult result = new SqlProcedureResult();
+				database.getConnection().createStatement().execute("set ANSI_WARNINGS off");
 				readSetups(service.getSystemFolder().resolve("setup").resolve("Setup.xml")//
 						, service.getSystemFolder().resolve("setup").resolve("dependency-graph.json")//
 						, service.getSystemFolder().resolve("setup")//
@@ -72,6 +73,7 @@ public class SetupService {
 
 				// Diese Methode darf erst ganz zum Schluss ausgeführt werden, damit sichergestellt werden kann, dass der Admin tatsächlich ALLE Rechte bekommt.
 				spc.setupPrivileges();
+				database.getConnection().createStatement().execute("set ANSI_WARNINGS on");
 				return new ResponseEntity(result, HttpStatus.ACCEPTED);
 			} catch (Exception e) {
 				throw new RuntimeException(e);
@@ -79,7 +81,7 @@ public class SetupService {
 		});
 		spc.registerExtensionBootstrapCheck(PROCEDURE_NAME, inputTable -> true);
 	}
-
+	
 	/**
 	 * Liest die setup-Dateien der Dependencies und gibt eine Liste an Strings mit den benötigten SQL-Dateien zurück.
 	 *

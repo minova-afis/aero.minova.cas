8731779cec5e183b7cbdfccf4f1cea1334c7283a
weber-minova <weber@minova.aero>
2023-06-26 11:26:51 +0200

    Aufruf korrigieren

--- service/src/main/java/aero/minova/cas/service/repository/ServicePropertiesRepository.java
+++ service/src/main/java/aero/minova/cas/service/repository/ServicePropertiesRepository.java
@@ -1,16 +1,13 @@
 package aero.minova.cas.service.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import aero.minova.cas.service.model.ServiceMessage;
 import aero.minova.cas.service.model.ServiceProperties;
 
 @Repository
 public interface ServicePropertiesRepository extends JpaRepository<ServiceProperties, Long> {
 
-	public List<ServiceMessage> findAllByServiceAndLastactionGreaterThan(String service, int lastaction);
+	public ServiceProperties findByServiceAndPropertyAndLastactionGreaterThan(String service, String property, int lastaction);
 
 }

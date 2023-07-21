9cb236431e5694760e227e6e4eeefa9696718acf
weber-minova <weber@minova.aero>
2023-07-21 15:08:47 +0200

    Korrekte Größe ist 200

--- service/src/main/java/aero/minova/cas/service/model/UserPrivilege.java
+++ service/src/main/java/aero/minova/cas/service/model/UserPrivilege.java
@@ -1,19 +1,46 @@
 package aero.minova.cas.service.model;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "xtcasUserPrivilege")
-public class UserPrivilege extends DataEntity {
+public class UserPrivilege {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "KeyLong")
+	private int keyLong;
+
+	@NotNull
+	@Size(max = 200)
+	@Column(name = "KeyText", length = 200)
+	public String keytext;
 
 	@Size(max = 100)
 	@Column(name = "TransactionChecker", length = 100)
 	public String transactionchecker;
+
+	@Column(name = "LastAction")
+	private Integer lastAction = 1;
+
+	@Size(max = 50)
+	@Column(name = "LastUser", length = 50)
+	String lastUser = "CAS_JPA";
+
+	@Column(name = "LastDate")
+	public Timestamp lastdate = Timestamp.from(Instant.now());
 
 }

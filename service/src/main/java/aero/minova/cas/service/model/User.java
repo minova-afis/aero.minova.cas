540597dd626d04920ac995f78ec6e245af74db98
weber-minova <weber@minova.aero>
2023-07-21 14:56:48 +0200

    doppelte Felder entfernen

--- service/src/main/java/aero/minova/cas/service/model/User.java
+++ service/src/main/java/aero/minova/cas/service/model/User.java
@@ -1,16 +1,11 @@
 package aero.minova.cas.service.model;
 
-import java.sql.Timestamp;
-import java.time.Instant;
-
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import lombok.Data;
 
 @Entity
@@ -27,19 +22,4 @@ public class User extends DataEntity {
 	@Size(max = 250)
 	@Column(name = "Memberships", length = 250)
 	public String memberships;
-
-	@NotNull
-	@Size(max = 50)
-	@Column(name = "LastUser", length = 50)
-	public String lastuser = "CAS_JPA";
-
-	@NotNull
-	@Column(name = "LastDate")
-	@Temporal(TemporalType.TIMESTAMP)
-	public Timestamp lastdate = Timestamp.from(Instant.now());
-
-	@NotNull
-	@Column(name = "LastAction")
-	public int lastaction = 1;
-
 }

f8f6923a04b850336fde0542b387f962700616f2
weber-minova <weber@minova.aero>
2023-09-12 11:40:48 +0200

    UserPrivilege von DataEntity trennen, da keyText länger sein muss

--- service/src/main/java/aero/minova/cas/service/model/UserPrivilege.java
+++ service/src/main/java/aero/minova/cas/service/model/UserPrivilege.java
@@ -1,16 +1,48 @@
 package aero.minova.cas.service.model;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "xtcasUserPrivilege")
-public class UserPrivilege extends DataEntity {
+public class UserPrivilege {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "KeyLong")
+	private int keyLong;
+
+	@NotNull
+	@Size(max = 200)
+	@Column(name = "KeyText", length = 200)
+	public String keyText;
+
+	@NotNull
+	@Size(max = 50)
+	@Column(name = "Description", length = 50)
+	public String description;
+
+	@Column(name = "LastAction")
+	private Integer lastAction = 1;
+
+	@Size(max = 50)
+	@Column(name = "LastUser", length = 50)
+	String lastUser = "CAS_JPA";
+
+	@Column(name = "LastDate")
+	public Timestamp lastDate = Timestamp.from(Instant.now());
 
 	@Size(max = 100)
 	@Column(name = "TransactionChecker", length = 100)

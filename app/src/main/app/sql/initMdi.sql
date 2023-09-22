aea875091913c1bc140699958bde7b0f378c9791
weber-minova <weber@minova.aero>
2023-09-22 12:16:05 +0200

    Rauten aus den UserToken-Felder entfernen

--- app/src/main/app/sql/initMdi.sql
+++ app/src/main/app/sql/initMdi.sql
@@ -2,14 +2,14 @@
 exec xpcasInitMdi null, 'CAS', '@CAS', null, null, null, 3, 'aero.minova.cas'
 
 -- Config Menu
-exec xpcasInitMdi 'config', null, '@cas.config', null, 10.0, '#admin', 2, 'aero.minova.cas'
-exec xpcasInitMdi 'ServiceProperties', 'xtcasServiceProperties', '@xtcasServiceProperties', 'config', 10.0, '#admin', 1, 'aero.minova.cas'
+exec xpcasInitMdi 'config', null, '@cas.config', null, 10.0, 'admin', 2, 'aero.minova.cas'
+exec xpcasInitMdi 'ServiceProperties', 'xtcasServiceProperties', '@xtcasServiceProperties', 'config', 10.0, 'admin', 1, 'aero.minova.cas'
 
 -- CAS Menu
-exec xpcasInitMdi 'CAS', null, '@CAS', null, 20.0, '#admin', 2, 'aero.minova.cas'
-exec xpcasInitMdi 'ColumnSecurity', 'ColumnSecurity', '@xtcasColumnSecurity', 'CAS', 11.0, '#admin', 1, 'aero.minova.cas'
-exec xpcasInitMdi 'UserGroup', 'UserGroup', '@xtcasUserGroup', 'CAS', 12.0, '#admin', 1, 'aero.minova.cas'
-exec xpcasInitMdi 'UserPrivilege', 'UserPrivilege', '@xtcasUserPrivilege', 'CAS', 13.0, '#admin', 1, 'aero.minova.cas'
-exec xpcasInitMdi 'Mdi', 'Menu', '@xtcasMdi', 'CAS', 14.0, '#admin', 1, 'aero.minova.cas'
-exec xpcasInitMdi 'User', 'LDAPUser', '@xtcasUser', 'CAS', 10.0, '#admin', 1, 'aero.minova.cas'
-exec xpcasInitMdi 'Users', 'DBUser', '@xtcasUsers', 'CAS', 10.0, '#admin', 1, 'aero.minova.cas'
+exec xpcasInitMdi 'CAS', null, '@CAS', null, 20.0, 'admin', 2, 'aero.minova.cas'
+exec xpcasInitMdi 'ColumnSecurity', 'ColumnSecurity', '@xtcasColumnSecurity', 'CAS', 11.0, 'admin', 1, 'aero.minova.cas'
+exec xpcasInitMdi 'UserGroup', 'UserGroup', '@xtcasUserGroup', 'CAS', 12.0, 'admin', 1, 'aero.minova.cas'
+exec xpcasInitMdi 'UserPrivilege', 'UserPrivilege', '@xtcasUserPrivilege', 'CAS', 13.0, 'admin', 1, 'aero.minova.cas'
+exec xpcasInitMdi 'Mdi', 'Menu', '@xtcasMdi', 'CAS', 14.0, 'admin', 1, 'aero.minova.cas'
+exec xpcasInitMdi 'User', 'LDAPUser', '@xtcasUser', 'CAS', 10.0, 'admin', 1, 'aero.minova.cas'
+exec xpcasInitMdi 'Users', 'DBUser', '@xtcasUsers', 'CAS', 10.0, 'admin', 1, 'aero.minova.cas'

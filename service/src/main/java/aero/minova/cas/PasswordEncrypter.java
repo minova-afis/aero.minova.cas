fe6e3749d7dce8bc9c8bacc708e215c21fb35f85
Stefan Flohr <flohr@MACFLS1.local>
2023-10-05 16:44:24 +0200

    Korrektur im bereich UserPribilege

--- service/src/main/java/aero/minova/cas/PasswordEncrypter.java
+++ service/src/main/java/aero/minova/cas/PasswordEncrypter.java
@@ -4,6 +4,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncrypter {
 	public static void main(String... args) {
-		System.out.println(new BCryptPasswordEncoder().encode("rqgzxTf71EAx8chvchMi"));
+		System.out.println(new BCryptPasswordEncoder().encode("S0M+2021"));
 	}
 }

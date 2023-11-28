807f5c1aa547dbc8ecef3a34d02b4ed9988793e4
Martins Avots <avots@minova.de>
2023-11-28 09:29:29 +0100

    #808 Resource-Listen erstellen.

--- resource-plugin/src/main/java/aero/minova/cas/resource/maven/plugin/ResourceListGenerator.java
+++ resource-plugin/src/main/java/aero/minova/cas/resource/maven/plugin/ResourceListGenerator.java
@@ -9,6 +9,7 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -18,13 +19,39 @@ public class ResourceListGenerator extends AbstractMojo {
     MavenProject project;
 
     public void execute() throws MojoExecutionException {
-        final var resourceFolder = Path.of(project.getBuild().getDirectory(), "classes", "aero.minova.app.resources");
+        final var classesFolder = Path.of(project.getBuild().getDirectory(), "classes");
+        final var resourceFolder = classesFolder.resolve("aero.minova.app.resources");
         final var resourceFile = resourceFolder.resolve(project.getGroupId() + "." + project.getArtifactId() + ".resources.txt");
         try {
             Files.createDirectories(resourceFolder);
-
+            final var resourceList = new StringBuilder();
+            for (String resourceSubFolderName : new String[]{"forms", "i18n", "images", "pdf", "plugins", "reports", "setup", "sql", "tables"}) {
+                final var resourceSubFolder = classesFolder.resolve(resourceSubFolderName);
+                if (Files.isDirectory(resourceSubFolder)) {
+                    try (final var resources = Files.walk(resourceSubFolder)) {
+                        resources.forEach(r -> {
+                            if (Files.isRegularFile(r)) {
+                                resourceList.append(normalize(classesFolder, r));
+                                resourceList.append('\n');
+                            } else if (Files.isDirectory(r)) {
+                                resourceList.append(normalize(classesFolder, r) + "/");
+                                resourceList.append('\n');
+                            } else {
+                                getLog().warn("Strange resource file: " + r);
+                            }
+                        });
+                    }
+                }
+            }
+            Files.writeString(resourceFile, resourceList.toString());
+            // TODO files als Sonderfall
         } catch (IOException e) {
             throw new MojoExecutionException("Could not generate resource file: " + resourceFile, e);
         }
+    }
+
+    private static String normalize(Path classesFolder, Path arg) {
+        return arg.toString().substring(classesFolder.toString().length())
+                .replace(FileSystems.getDefault().getSeparator(), "/");
     }
 }

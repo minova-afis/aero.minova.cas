eccbd322053af6b874a93fe19cc714caacee570f
janiak <janiak@minova.aero>
2023-12-15 10:48:27 +0100

    Repositories für Mdi und MdiType

---
+++ service/src/main/java/aero/minova/cas/service/repository/MdiRepository.java
@@ -0,0 +1,7 @@
+package aero.minova.cas.service.repository;
+
+import aero.minova.cas.service.model.Mdi;
+
+public interface MdiRepository extends DataEntityRepository<Mdi> {
+
+}
---
+++ service/src/main/java/aero/minova/cas/service/repository/MdiTypeRepository.java
@@ -0,0 +1,7 @@
+package aero.minova.cas.service.repository;
+
+import aero.minova.cas.service.model.MdiType;
+
+public interface MdiTypeRepository extends DataEntityRepository<MdiType> {
+
+}

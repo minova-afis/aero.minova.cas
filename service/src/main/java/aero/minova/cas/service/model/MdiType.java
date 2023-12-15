2aa0157db44726aeecb6831abfbe61d24f0d3fa5
janiak <janiak@minova.aero>
2023-12-15 11:46:56 +0100

    MdiType um Beschreibung erweitern

--- service/src/main/java/aero/minova/cas/service/model/MdiType.java
+++ service/src/main/java/aero/minova/cas/service/model/MdiType.java
@@ -1,5 +1,8 @@
 package aero.minova.cas.service.model;
 
+import javax.validation.constraints.Size;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -8,5 +11,9 @@ import lombok.Data;
 @Data
 @Table(name = "xtcasMdiType")
 public class MdiType extends DataEntity {
+
+	@Size(max = 50)
+	@Column(name = "Description", length = 50)
+	public String description;
 
 }

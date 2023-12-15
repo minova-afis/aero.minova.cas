04ca28af302d1f18e3d6883f3bf1bdbc20a4c03a
janiak <janiak@minova.aero>
2023-12-15 10:48:46 +0100

    Konstruktor für Mdi

--- service/src/main/java/aero/minova/cas/service/model/Mdi.java
+++ service/src/main/java/aero/minova/cas/service/model/Mdi.java
@@ -8,10 +8,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "xtcasMdi")
 public class Mdi extends DataEntity {
 

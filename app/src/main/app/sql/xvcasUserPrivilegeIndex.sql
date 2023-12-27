ccf7fbb6854a86bd2211faa037ccd658a1f71c2c
janiak <janiak@minova.aero>
2023-12-27 15:43:57 +0100

    View anpassen

--- app/src/main/app/sql/xvcasUserPrivilegeIndex.sql
+++ app/src/main/app/sql/xvcasUserPrivilegeIndex.sql
@@ -9,4 +9,6 @@ with encryption as
 	from xtcasUserPrivilege up
 	left outer join xtcasLuUserPrivilegeUserGroup lu on lu.UserPrivilegeKey = up.KeyLong
 	left outer join xtcasUserGroup ug on ug.KeyLong=lu.UserGroupKey
-	where up.LastAction > 0
+	where up.LastAction > 0 
+		and lu.LastAction > 0
+		and ug.LastAction > 0

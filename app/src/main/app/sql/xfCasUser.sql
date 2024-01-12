9f6dacdbc5b0085d8e546e835128b1e89b7d9875
erlanger <erlanger@localhost>
2024-01-12 10:54:35 +0100

    donwngrade funktion to SQL Server 2012

--- app/src/main/app/sql/xfCasUser.sql
+++ app/src/main/app/sql/xfCasUser.sql
@@ -1,5 +1,5 @@
 alter function dbo.xfCasUser()
 returns nvarchar(100) as
 begin
-	return (select COALESCE(CONVERT(nvarchar(100), (select session_context(N'casUser'))), 'unknown-user'))
+	return 'unknown-user_SQL20122Old'
 end

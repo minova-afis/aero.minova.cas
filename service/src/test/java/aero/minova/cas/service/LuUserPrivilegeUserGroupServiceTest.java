db9cbd4240602b8a086b16dbf40be56bb848819d
janiak <janiak@minova.aero>
2024-02-07 15:52:10 +0100

    Test unabhängig machen

--- service/src/test/java/aero/minova/cas/service/LuUserPrivilegeUserGroupServiceTest.java
+++ service/src/test/java/aero/minova/cas/service/LuUserPrivilegeUserGroupServiceTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 
 import aero.minova.cas.CoreApplicationSystemApplication;
@@ -15,6 +17,7 @@ import aero.minova.cas.service.model.UserPrivilege;
 
 @SpringBootTest(classes = CoreApplicationSystemApplication.class)
 @ActiveProfiles("test")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 class LuUserPrivilegeUserGroupServiceTest {
 
 	@Autowired

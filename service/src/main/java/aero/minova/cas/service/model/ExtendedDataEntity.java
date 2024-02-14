436ea972a9c66c6973a93f0689c46b8e6b573070
janiak <janiak@minova.aero>
2024-02-14 11:20:21 +0100

    ExtendedDataEntity Klasse für Entities, die auch eine Beschreibung haben

--- service/src/main/java/aero/minova/cas/service/model/ExtendedDataEntity.java
+++ service/src/main/java/aero/minova/cas/service/model/ExtendedDataEntity.java
@@ -12,7 +12,11 @@ import lombok.ToString;
 @ToString(callSuper = true)
 public abstract class ExtendedDataEntity extends DataEntity {
 
+<<<<<<< jal-extended_data_entity
 	@Column(length = 50)
+=======
+	@Column(length = 200)
+>>>>>>> 8e9d292 ExtendedDataEntity Klasse für Entities, die auch eine Beschreibung haben
 	private String description;
 
 }

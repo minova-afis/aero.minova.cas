5ead601b3719a79e1695a34eb4a6b84c3107a99e
Lea Janiak <janiak@minova.aero>
2024-07-12 08:26:25 +0200

    RF: Nicht genutzten Code entfernen

--- service/src/main/java/aero/minova/cas/SecurityConfig.java
+++ service/src/main/java/aero/minova/cas/SecurityConfig.java
@@ -25,9 +25,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
-import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
-import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -82,8 +79,7 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		DelegatingServerLogoutHandler logoutHandler = new DelegatingServerLogoutHandler(new WebSessionServerLogoutHandler(),
-				new SecurityContextServerLogoutHandler());
+
 		http.authorizeHttpRequests(requests -> requests
 				.requestMatchers("/actuator/**").permitAll().requestMatchers("/", "/public/**", "/img/**", "/js/**", "/theme/**", "/index", "/login", "/layout")
 				.permitAll().anyRequest().fullyAuthenticated()).logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/"))

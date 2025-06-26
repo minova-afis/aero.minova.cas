f203e976bdf135434cbec07756917aab3720fab5
Martins Avots <avots@minova.de>
2025-06-26 11:02:38 +0200

    CAS bei fehlenden Speicher beenden
    https://github.com/minova-afis/aero.minova.cas/issues/1300

--- service/src/main/java/aero/minova/cas/service/SelfProbingService.java
+++ service/src/main/java/aero/minova/cas/service/SelfProbingService.java
@@ -1,14 +1,28 @@
 package aero.minova.cas.service;
 
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.annotation.PostConstruct;
+import org.hibernate.Session;
+import org.hibernate.internal.SessionFactoryImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class SelfProbingService {
 
+	@PostConstruct
+	public void setup() {
+		Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
+			if (e instanceof OutOfMemoryError) {
+				System.exit(1);
+			}
+		});
+	}
+
 	@Scheduled(cron = "${self.probing.cron:0 * * * * *}")
 	private void run() {
-
 	}
 }

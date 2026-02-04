16eed7f9a7df1b51c4f976831603845cc818766f
Lea Janiak <janiak@minova.aero>
2026-02-04 10:02:01 +0100

    CAS-Menüs weiter hinten anordnen

--- app/src/main/app/sql/initMdi.sql
+++ app/src/main/app/sql/initMdi.sql
@@ -2,11 +2,11 @@
 exec xpcasInitMdi null, 'CAS', '@CAS', null, null, null, 3, 'aero.minova.cas'
 
 -- Config Menu
-exec xpcasInitMdi 'config', null, '@cas.config', null, 10.0, 'admin', 2, 'aero.minova.cas'
+exec xpcasInitMdi 'config', null, '@cas.config', null, 100.0, 'admin', 2, 'aero.minova.cas'
 exec xpcasInitMdi 'ServiceProperties', 'xtcasServiceProperties', '@xtcasServiceProperties', 'config', 10.0, 'admin', 1, 'aero.minova.cas'
 
 -- CAS Menu
-exec xpcasInitMdi 'CAS', null, '@CAS', null, 20.0, 'admin', 2, 'aero.minova.cas'
+exec xpcasInitMdi 'CAS', null, '@CAS', null, 110.0, 'admin', 2, 'aero.minova.cas'
 exec xpcasInitMdi 'ColumnSecurity', 'ColumnSecurity', '@xtcasColumnSecurity', 'CAS', 11.0, 'admin', 1, 'aero.minova.cas'
 exec xpcasInitMdi 'UserGroup', 'UserGroup', '@xtcasUserGroup', 'CAS', 12.0, 'admin', 1, 'aero.minova.cas'
 exec xpcasInitMdi 'UserPrivilege', 'UserPrivilege', '@xtcasUserPrivilege', 'CAS', 13.0, 'admin', 1, 'aero.minova.cas'

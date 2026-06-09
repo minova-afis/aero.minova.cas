234820f985e800ce507a787dbce22a7e0b5ee6e8
Lea Janiak <janiak@minova.aero>
2026-06-09 11:27:08 +0200

    Methode ohne annotations

--- service/src/main/java/aero/minova/cas/controller/FilesController.java
+++ service/src/main/java/aero/minova/cas/controller/FilesController.java
@@ -172,8 +172,7 @@ public class FilesController {
 		return dbFileService.getMD5(path);
 	}
 
-	@RequestMapping(value = "files/read", produces = { MediaType.APPLICATION_OCTET_STREAM_VALUE })
-	public @ResponseBody byte[] getFile(@RequestParam String path) throws Exception {
+	public byte[] getFile(String path) throws Exception {
 		return getFile(path, null);
 	}
 
@@ -266,8 +265,7 @@ public class FilesController {
 		return readAllBytes(inputPath);
 	}
 
-	@RequestMapping(value = "files/hash", produces = { MediaType.APPLICATION_OCTET_STREAM_VALUE })
-	public @ResponseBody byte[] getHash(@RequestParam String path) throws Exception {
+	public byte[] getHash(String path) throws Exception {
 		return getHash(path, null);
 	}
 
